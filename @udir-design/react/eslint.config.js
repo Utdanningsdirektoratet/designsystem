a57f2b3c955a38139857eeacfb7f2986d22eec7d
Une Kinn Ekroll <une.kinn.ekroll@bekk.no>
2024-06-21 17:10:56 +0200

    build: fix eslint errors after storybook 8 by using new flat config

---
+++ @udir-design/react/eslint.config.js
@@ -0,0 +1,36 @@
+import { FlatCompat } from '@eslint/eslintrc';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import baseConfig from '../../eslint.config.js';
+import eslintjs from '@eslint/js';
+const { configs } = eslintjs;
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: configs.recommended,
+});
+
+export default [
+  ...baseConfig,
+  ...compat.extends('plugin:@nx/react'),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+    languageOptions: {
+      parserOptions: {
+        project: [
+          '@udir-design/react/tsconfig.*?.json',
+          '@udir-design/react/tsconfig.storybook.json',
+        ],
+      },
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  { ignores: ['storybook-static'] },
+];
---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import nxEslintPlugin from '@nx/eslint-plugin';
+import eslintjs from '@eslint/js';
+const { configs } = eslintjs;
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: configs.recommended,
+});
+
+export default [
+  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+];
