c2328d35af2c054606088f260d03d82d1bfe9c13
Une Kinn Ekroll <une.kinn.ekroll@bekk.no>
2024-08-23 11:30:54 +0200

    chore: add a Storybook indexer which uses @internal/dynamic-csf to autogenerate stories for variants

--- @udir-design/react/.storybook/main.ts
+++ @udir-design/react/.storybook/main.ts
@@ -2,12 +2,21 @@ import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../src/**/*.dynamic.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     'storybook-addon-pseudo-states',
     '@chromatic-com/storybook',
+    {
+      name: './dynamic-indexer/preset',
+      options: {
+        tsconfigPath: 'tsconfig.storybook.json',
+      },
+    },
   ],
 
   framework: {
@@ -21,11 +30,19 @@ const config: StorybookConfig = {
 
   docs: {},
 
+  tags: {
+    // Configure stories with the 'snapshot' tag to only be visible in development.
+    // In production, they will be picked up by snapshot tests etc but not be visible.
+    snapshot: {
+      excludeFromDocsStories: false,
+      excludeFromSidebar: process.env.NODE_ENV !== 'development',
+    },
+  },
+
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
 };
-
 export default config;
 
 // To customize your Vite configuration you can use the viteFinal field.
