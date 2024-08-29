5549748feb9a605b9c0290e30b47fa2fe67aac02
Une Kinn Ekroll <une.kinn.ekroll@bekk.no>
2024-08-29 17:08:15 +0200

    build: fix lint task randomly failing with 'Could not find plugin "@typescript-eslint"'

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import nxEslintPlugin from '@nx/eslint-plugin';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import eslintjs from '@eslint/js';
 const { configs } = eslintjs;
 
@@ -9,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 export default [
-  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    plugins: {
+      '@nx': nxEslintPlugin,
+      // This line should be unnecessary, but eslint randomly fails with
+      // > TypeError: Key "rules": Key "@typescript-eslint/no-array-constructor":
+      // >   Could not find plugin "@typescript-eslint".
+      '@typescript-eslint': tsPlugin,
+    },
+  },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
