dce7da1fe9b5e3670246563be8c9618529bd4bc1
Guro Brøto <guro.broto@bekk.no>
2024-11-12 09:50:44 +0100

    feat: add breadcrumbs component from digdir

---
+++ @udir-design/react/src/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,10 @@
+export {
+  Breadcrumbs,
+  type BreadcrumbsProps,
+  BreadcrumbsItem,
+  type BreadcrumbsItemProps,
+  BreadcrumbsLink,
+  type BreadcrumbsLinkProps,
+  BreadcrumbsList,
+  type BreadcrumbsListProps,
+} from '@digdir/designsystemet-react';
