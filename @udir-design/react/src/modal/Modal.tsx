8fcc4e49759655332db4a9a65aeeb3dd0fec2c80
Guro Brøto <guro.broto@bekk.no>
2024-11-12 13:00:57 +0100

    feat: add modal component from digdir

---
+++ @udir-design/react/src/modal/Modal.tsx
@@ -0,0 +1,10 @@
+export {
+  Modal,
+  type ModalProps,
+  ModalBlock,
+  type ModalBlockProps,
+  ModalContext,
+  type ModalContextProps,
+  ModalTrigger,
+  type ModalTriggerProps,
+} from '@digdir/designsystemet-react';
