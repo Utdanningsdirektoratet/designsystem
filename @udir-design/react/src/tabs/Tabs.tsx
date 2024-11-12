762b2eeed7df5a7db5e4c8c92484f98e03fbed80
Guro Brøto <guro.broto@bekk.no>
2024-11-12 13:52:15 +0100

    feat: add tabs component from digdir

---
+++ @udir-design/react/src/tabs/Tabs.tsx
@@ -0,0 +1,10 @@
+export {
+  Tabs,
+  type TabsProps,
+  TabsTab,
+  type TabsTabProps,
+  TabsPanel,
+  type TabsPanelProps,
+  TabsList,
+  type TabsListProps,
+} from '@digdir/designsystemet-react';
