2cbd58e180c75a28f700250877fa737e66f3f318
Guro Brøto <guro.broto@bekk.no>
2024-11-12 13:04:16 +0100

    feat: add pagination component from digdir

---
+++ @udir-design/react/src/pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+export {
+  Pagination,
+  type PaginationProps,
+  PaginationButton,
+  type PaginationButtonProps,
+  PaginationItem,
+  type PaginationItemProps,
+  PaginationList,
+  type PaginationListProps,
+  usePagination,
+  type UsePaginationProps,
+} from '@digdir/designsystemet-react';
