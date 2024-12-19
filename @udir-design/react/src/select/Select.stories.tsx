b590403a661fdfdbb587a90c20c6a7001b65bee0
Guro Brøto <guro.broto@bekk.no>
2024-12-19 12:01:07 +0100

    fix: fix Select story

--- @udir-design/react/src/select/Select.stories.tsx
+++ @udir-design/react/src/select/Select.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Select } from './Select';
+import { Label } from '../typography/label/Label';
+import { Field } from '../field/Field';
 
 const meta: Meta<typeof Select> = {
   component: Select,
@@ -10,16 +12,19 @@ type Story = StoryObj<typeof Select>;
 
 export const Preview: Story = {
   render: (args) => (
-    <Select {...args}>
-      <Select.Option value="blank">Velg …</Select.Option>
-      <Select.Option value="everest">Mount Everest</Select.Option>
-      <Select.Option value="aconcagua">Aconcagua</Select.Option>
-      <Select.Option value="denali">Denali</Select.Option>
-      <Select.Option value="kilimanjaro">Kilimanjaro</Select.Option>
-      <Select.Option value="elbrus">Elbrus</Select.Option>
-      <Select.Option value="vinson">Mount Vinson</Select.Option>
-      <Select.Option value="puncakjaya">Puncak Jaya</Select.Option>
-      <Select.Option value="kosciuszko">Mount Kosciuszko</Select.Option>
-    </Select>
+    <Field>
+      <Label>Velg et fjell</Label>
+      <Select {...args}>
+        <Select.Option value="blank">Velg …</Select.Option>
+        <Select.Option value="everest">Mount Everest</Select.Option>
+        <Select.Option value="aconcagua">Aconcagua</Select.Option>
+        <Select.Option value="denali">Denali</Select.Option>
+        <Select.Option value="kilimanjaro">Kilimanjaro</Select.Option>
+        <Select.Option value="elbrus">Elbrus</Select.Option>
+        <Select.Option value="vinson">Mount Vinson</Select.Option>
+        <Select.Option value="puncakjaya">Puncak Jaya</Select.Option>
+        <Select.Option value="kosciuszko">Mount Kosciuszko</Select.Option>
+      </Select>
+    </Field>
   ),
 };
