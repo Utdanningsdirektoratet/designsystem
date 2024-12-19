b91fc6e63076339bdb3e8875d970ec680fee524d
Guro Brøto <guro.broto@bekk.no>
2024-12-19 12:01:31 +0100

    fix: fix textfield story

--- @udir-design/react/src/textfield/Textfield.stories.tsx
+++ @udir-design/react/src/textfield/Textfield.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Textfield } from './Textfield';
+import { Label } from '../typography/label/Label';
 
 const meta: Meta<typeof Textfield> = {
   component: Textfield,
@@ -9,5 +10,10 @@ export default meta;
 type Story = StoryObj<typeof Textfield>;
 
 export const Preview: Story = {
-  args: {},
+  render: (args) => (
+    <>
+      <Label htmlFor="my-textfield">Label</Label>
+      <Textfield id="my-textfield" {...args} />
+    </>
+  ),
 };
