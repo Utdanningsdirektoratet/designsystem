ad046d77a2dc3f1a2ca129fac7885b12224d5fad
Guro Brøto <guro.broto@bekk.no>
2025-01-02 12:57:41 +0100

    refactor: change folder name from kebab-case to camel-case for ValidationMessage

--- @udir-design/react/src/typography/index.tsx
+++ @udir-design/react/src/typography/index.tsx
@@ -1,4 +1,4 @@
 export * from './heading/Heading';
 export * from './label/Label';
 export * from './paragraph/Paragraph';
-export * from './validation-message/ValidationMessage';
+export * from './validationMessage/ValidationMessage';
