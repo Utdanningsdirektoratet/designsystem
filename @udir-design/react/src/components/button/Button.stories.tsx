2013e4db8b0b65aca7dc6dba31acbf7c35fc33ca
Guro Brøto <guro.broto@bekk.no>
2025-01-06 13:35:32 +0100

    refactor: put all components in react-library into a components folder in src

--- @udir-design/react/src/components/button/Button.stories.tsx
+++ @udir-design/react/src/components/button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { within, expect } from '@storybook/test';
 import { Button } from './Button';
-import { spaceForFocusStyleDecorator } from '../../.storybook/utils/spaceForFocusStyleDecorator';
+import { spaceForFocusStyleDecorator } from '../../../.storybook/utils/spaceForFocusStyleDecorator';
 
 const meta: Meta<typeof Button> = {
   component: Button,
