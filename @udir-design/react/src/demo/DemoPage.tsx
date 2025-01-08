634b029780b6c20b560b262aad0bbab75d2ac089
Guro Brøto <guro.broto@bekk.no>
2025-01-08 12:57:32 +0100

    feat: add demo-page to storybook with three examples

---
+++ @udir-design/react/src/demo/DemoPage.tsx
@@ -0,0 +1,19 @@
+import { FrequentQuestions } from './frequent-questions/FrequentQuestions';
+import { Login } from './login/Login';
+import { Shoppinglist } from './shoppinglist/Shoppinglist';
+import styles from './DemoPage.module.scss';
+
+export type Props = {
+  'data-color-scheme': 'auto' | 'dark' | 'light'; // TODO: type should come from @digdir, must be added there
+  'data-size': 'sm' | 'md' | 'lg'; // TODO: type should come from @digdir, does not work as hoped to use Size from @digdir
+};
+
+export function DemoPage(props: Props) {
+  return (
+    <div className={styles.page} {...props}>
+      <Shoppinglist />
+      <Login />
+      <FrequentQuestions />
+    </div>
+  );
+}
