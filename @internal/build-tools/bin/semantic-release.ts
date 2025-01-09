5c2840b9f7ed0e1eceb8eabddccfaf34075571f4
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-01-09 17:35:06 +0100

    ci: add build scripts and workflow to automate the release process

---
+++ @internal/build-tools/bin/semantic-release.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env -S pnpm tsx
+
+import process from 'node:process';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { semanticRelease } from '../src/semantic-release';
+
+(async () => {
+  const options = await yargs(hideBin(process.argv))
+    .version(false) // disable default --version option in yargs
+    .options({
+      branch: {
+        alias: 'b',
+        description:
+          'The branch name is used to determine the correct release strategy',
+        type: 'string',
+        demandOption: true,
+      },
+      dryRun: {
+        alias: 'd',
+        description:
+          'Whether or not to perform a dry-run of the entire release process',
+        type: 'boolean',
+        default: true,
+      },
+      previewChangelog: {
+        description:
+          'Similar to dry-run, but will actually update version numbers and changelogs (without committing to git). Useful for previewing the changelogs in CI.',
+        type: 'boolean',
+        default: false,
+      },
+      publish: {
+        description: 'Whether or not to run the final publish step.',
+        type: 'boolean',
+        default: false,
+      },
+      commitVersionNumbers: {
+        description:
+          'Whether or not to commit changes in package.json "version" fields to git. This is NOT recommended, see https://semantic-release.gitbook.io/semantic-release/support/faq#why-is-the-package.jsons-version-not-updated-in-my-repository',
+        type: 'boolean',
+        default: false,
+      },
+      commitChangelog: {
+        description:
+          'Whether or not to commit changes in changelog files to git. This is NOT recommended, see https://semantic-release.gitbook.io/semantic-release/support/faq#should-release-notes-be-committed-to-a-changelog.md-in-my-repository-during-a-release',
+        type: 'boolean',
+        default: false,
+      },
+      gitPush: {
+        description:
+          'Whether or not to push git tags (and version / changelog commits, if enabled) to the remote',
+        type: 'boolean',
+        default: true,
+      },
+      verbose: {
+        description: 'Whether or not to enable verbose logging',
+        type: 'boolean',
+        default: false,
+      },
+    })
+    .parseAsync();
+
+  await semanticRelease(options);
+})();
