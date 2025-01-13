8e1a7887bb082e6cbc7790ecd2112a5ce54d93a1
Guro Brøto <guro.broto@bekk.no>
2025-01-13 12:22:29 +0100

    feat: add heading and paragraph examples to demo page

--- @udir-design/react/src/demo/DemoPage.tsx
+++ @udir-design/react/src/demo/DemoPage.tsx
@@ -3,6 +3,7 @@ import { Login } from './login/Login';
 import { Shoppinglist } from './shoppinglist/Shoppinglist';
 import styles from './DemoPage.module.scss';
 import { UserTable } from './user-table/UserTable';
+import { Heading, Paragraph } from '../components/';
 
 export type Props = {
   'data-color-scheme': 'auto' | 'dark' | 'light'; // TODO: type should come from @digdir, must be added there
@@ -12,10 +13,21 @@ export type Props = {
 export function DemoPage(props: Props) {
   return (
     <div className={styles.page} {...props}>
-      <Shoppinglist />
-      <Login />
-      <FrequentQuestions />
-      <UserTable />
+      <Heading>Velkommen! </Heading>
+      <Paragraph>
+        På denne demosiden hvor vi har laget noen eksempler på komponentene i
+        biblioteket, sånn at man kan se hvordan de samspiller i en litt større
+        kontekst. På sikt vil alle komponentene være representert her, men så
+        langt er det kun de vi har ansett som viktigst i første omgang. Lengre
+        ned på siden kan du prøve å endre på størrelse og farge-modus for å se
+        hvordan det påvirker komponentene.
+      </Paragraph>
+      <div className={styles.examples}>
+        <Shoppinglist />
+        <Login />
+        <FrequentQuestions />
+        <UserTable />
+      </div>
     </div>
   );
 }
