c1a9959bf1d391b16ebc846c126e3d2cdf301d93
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-01-16 14:47:07 +0100

    test: without dynamic stories we don't need --index-json and can co-locate snapshots and components

--- @udir-design/react/test-runner-jest.config.js
+++ @udir-design/react/test-runner-jest.config.js
@@ -6,7 +6,6 @@ const defaultConfig = getJestConfig();
 const config = {
   ...defaultConfig,
   rootDir: './',
-  snapshotResolver: './snapshot-resolver.js',
   snapshotSerializers: [
     // Sets up the custom serializer to preprocess the HTML before it's passed onto the test-runner
     './snapshot-serializer.js',
