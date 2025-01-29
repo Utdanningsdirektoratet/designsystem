6b1d7016a3d3f28ca4f5c30b34552f1bb0fe41ff
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-01-29 14:39:38 +0100

    docs: fix IncludeMarkdown to not look weird with new styling

--- @udir-design/react/.storybook/preview.tsx
+++ @udir-design/react/.storybook/preview.tsx
@@ -3,6 +3,7 @@ import { Preview } from '@storybook/react';
 import type { MarkdownToJSX } from 'markdown-to-jsx';
 import customTheme from './customTheme';
 import { Heading, Link, LinkProps, List, Paragraph, Table } from '../src/alpha';
+import componentStyles from './componentOverrides.module.scss';
 
 type Props = Record<string, unknown>;
 
@@ -24,50 +25,32 @@ const getPath = (href: string | undefined): string => {
 export const componentOverrides = {
   h1: (props: Props) => (
     <Heading
-      {...props}
-      className="sb-unstyled"
-      level={1}
       data-size="lg"
-      data-color-scheme="light"
-      style={{
-        backgroundColor: 'transparent',
-        marginBottom: 'var(--ds-spacing-2)',
-      }}
+      {...props}
+      className={`sb-unstyled ${componentStyles.heading}`}
+      level={1}
     />
   ),
   h2: (props: Props) => (
     <Heading
-      {...props}
-      className="sb-unstyled"
-      level={2}
       data-size="md"
-      data-color-scheme="light"
-      style={{
-        backgroundColor: 'transparent',
-        marginTop: 'var(--ds-spacing-5)',
-        marginBottom: 'var(--ds-spacing-2)',
-      }}
+      {...props}
+      className={`sb-unstyled ${componentStyles.heading}`}
+      level={2}
     />
   ),
   h3: (props: Props) => (
     <Heading
-      {...props}
-      className="sb-unstyled"
-      level={3}
       data-size="sm"
-      data-color-scheme="light"
-      style={{
-        backgroundColor: 'transparent',
-        marginTop: 'var(--ds-spacing-4)',
-        marginBottom: 'var(--ds-spacing-2)',
-      }}
+      {...props}
+      className={`sb-unstyled ${componentStyles.heading}`}
+      level={3}
     />
   ),
   p: (props: Props) => (
     <Paragraph
       {...props}
       className="sb-unstyled"
-      data-color-scheme="light"
       style={{
         backgroundColor: 'transparent',
         marginBottom: 'var(--ds-spacing-2)',
@@ -79,7 +62,6 @@ export const componentOverrides = {
       {...props}
       style={{ maxWidth: '70ch', marginBottom: 'var(--ds-spacing-2)' }}
       className="sb-unstyled"
-      data-color-scheme="light"
     />
   ),
   ul: (props: Props) => (
@@ -87,7 +69,6 @@ export const componentOverrides = {
       {...props}
       style={{ maxWidth: '70ch', marginBottom: 'var(--ds-spacing-2)' }}
       className="sb-unstyled"
-      data-color-scheme="light"
     />
   ),
   li: (props: Props) => (
@@ -95,50 +76,23 @@ export const componentOverrides = {
       {...props}
       className="sb-unstyled"
       style={{ maxWidth: '70ch', marginTop: 'var(--ds-spacing-1)' }}
-      data-color-scheme="light"
     />
   ),
   a: (props: LinkProps) => {
     // if link starts with /, add current path to link
     const href = getPath(props.href);
 
-    return (
-      <Link
-        {...props}
-        href={href}
-        className="sb-unstyled"
-        data-color-scheme="light"
-      />
-    );
+    return <Link {...props} href={href} className="sb-unstyled" />;
   },
   table: (props: Props) => (
-    <Table
-      {...props}
-      zebra
-      className="sb-unstyled"
-      style={{ width: '100%' }}
-      data-color-scheme="light"
-    />
+    <Table {...props} zebra className="sb-unstyled" style={{ width: '100%' }} />
   ),
-  thead: (props: Props) => (
-    <Table.Head {...props} className="sb-unstyled" data-color-scheme="light" />
-  ),
-  tbody: (props: Props) => (
-    <Table.Body {...props} className="sb-unstyled" data-color-scheme="light" />
-  ),
-  tr: (props: Props) => (
-    <Table.Row {...props} className="sb-unstyled" data-color-scheme="light" />
-  ),
-  th: (props: Props) => (
-    <Table.HeaderCell
-      {...props}
-      className="sb-unstyled"
-      data-color-scheme="light"
-    />
-  ),
-  td: (props: Props) => (
-    <Table.Cell {...props} className="sb-unstyled" data-color-scheme="light" />
-  ),
+  thead: (props: Props) => <Table.Head {...props} className="sb-unstyled" />,
+  tbody: (props: Props) => <Table.Body {...props} className="sb-unstyled" />,
+  tr: (props: Props) => <Table.Row {...props} className="sb-unstyled" />,
+  th: (props: Props) => <Table.HeaderCell {...props} className="sb-unstyled" />,
+  td: (props: Props) => <Table.Cell {...props} className="sb-unstyled" />,
+  dl: (props: Props) => <dl {...props} className="sb-unstyled" />,
 } satisfies MarkdownToJSX.Overrides;
 
 const preview: Preview = {
