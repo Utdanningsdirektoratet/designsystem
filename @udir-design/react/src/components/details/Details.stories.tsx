5d4334878654c2fc901c5f86af420af47d1d4753
Adrian Flatner <adrianrflatner@gmail.com>
2025-01-30 14:50:52 +0100

    docs: remove comment

--- @udir-design/react/src/components/details/Details.stories.tsx
+++ @udir-design/react/src/components/details/Details.stories.tsx
@@ -1,27 +1,3 @@
-// import type { Meta, StoryObj } from '@storybook/react';
-// import { Details } from './Details';
-
-// const meta: Meta<typeof Details> = {
-//   component: Details,
-//   tags: ['alpha'],
-// };
-
-// export default meta;
-// type Story = StoryObj<typeof Details>;
-
-// export const Preview: Story = {
-//   render: (args) => (
-//     <Details {...args}>
-//       <Details.Summary>Fagskole</Details.Summary>
-//       <Details.Content>
-//         Fagskoler kan tilby både etter- og videreutdanning. Fagskoler er
-//         praksisrettede og har tette bånd til arbeidslivet, og kan tilby
-//         utdanninger på særlig fagspesifikke tema.
-//       </Details.Content>
-//     </Details>
-//   ),
-// };
-
 import type { Meta, StoryFn } from '@storybook/react';
 import { useState } from 'react';
 
