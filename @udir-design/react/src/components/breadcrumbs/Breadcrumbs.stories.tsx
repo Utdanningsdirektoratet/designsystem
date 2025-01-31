2486e665b6f7f0d64fc54973423e7b910138a5a4
Adrian Flatner <adrianrflatner@gmail.com>
2025-01-31 13:02:02 +0100

    docs: updating to using storyObj

--- @udir-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
+++ @udir-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,17 +1,20 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Breadcrumbs } from './Breadcrumbs';
 
 export default {
   component: Breadcrumbs,
   tags: ['alpha'],
-} as Meta;
+} as Meta<typeof Breadcrumbs>;
 
-export const Preview: StoryFn<typeof Breadcrumbs> = (args) => (
-  <>
-    <Breadcrumbs aria-label="Du er her:" {...args}>
+type Story = StoryObj<typeof Breadcrumbs>;
+
+export const Preview: Story = {
+  args: {
+    'aria-label': 'Du er her:',
+    children: [
       <Breadcrumbs.Link href="#" aria-label="Tilbake til Nivå 3">
         Nivå 3
-      </Breadcrumbs.Link>
+      </Breadcrumbs.Link>,
       <Breadcrumbs.List>
         <Breadcrumbs.Item>
           <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
@@ -25,104 +28,115 @@ export const Preview: StoryFn<typeof Breadcrumbs> = (args) => (
         <Breadcrumbs.Item>
           <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
         </Breadcrumbs.Item>
-      </Breadcrumbs.List>
-    </Breadcrumbs>
-  </>
-);
+      </Breadcrumbs.List>,
+    ],
+  },
+};
 
-export const ListOnly: StoryFn<typeof Breadcrumbs> = (args) => (
-  <Breadcrumbs aria-label="Du er her:">
-    <Breadcrumbs.List>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-    </Breadcrumbs.List>
-  </Breadcrumbs>
-);
+export const ListOnly: Story = {
+  args: {
+    'aria-label': 'Du er her:',
+    children: [
+      <Breadcrumbs.List>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+      </Breadcrumbs.List>,
+    ],
+  },
+};
 
-export const BackOnly: StoryFn<typeof Breadcrumbs> = (args) => (
-  <Breadcrumbs>
-    <Breadcrumbs.Link href="#" aria-label="Tilbake til Nivå 3">
-      Nivå 3
-    </Breadcrumbs.Link>
-  </Breadcrumbs>
-);
+export const BackOnly: Story = {
+  args: {
+    children: [
+      <Breadcrumbs.Link href="#" aria-label="Tilbake til Nivå 3">
+        Nivå 3
+      </Breadcrumbs.Link>,
+    ],
+  },
+};
 
-export const LongItems: StoryFn<typeof Breadcrumbs> = (args) => (
-  <Breadcrumbs {...args}>
-    <Breadcrumbs.Link
-      href="#"
-      aria-label="Tilbake til helsesertifikat for sjømat"
-    >
-      Slik søker du om helsesertifikat for sjømat
-    </Breadcrumbs.Link>
-    <Breadcrumbs.List aria-label="Du er her:">
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Hjem</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">
-          Eksport til land utenfor EU/EØS
-        </Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Eksport av mat og drikke</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Eksport av fisk og sjømat</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">
-          Veiledning om helsesertifikat for sjømat
-        </Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">
-          Slik søker du om helsesertifikat for sjømat
-        </Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">
-          Slik søker du om helsesertifikat i ny eksportløsning
-        </Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-    </Breadcrumbs.List>
-  </Breadcrumbs>
-);
+export const LongItems: Story = {
+  args: {
+    children: [
+      <Breadcrumbs.Link
+        href="#"
+        aria-label="Tilbake til helsesertifikat for sjømat"
+      >
+        Slik søker du om helsesertifikat for sjømat
+      </Breadcrumbs.Link>,
+      <Breadcrumbs.List aria-label="Du er her:">
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Hjem</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">
+            Eksport til land utenfor EU/EØS
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Eksport av mat og drikke</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">
+            Eksport av fisk og sjømat
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">
+            Veiledning om helsesertifikat for sjømat
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">
+            Slik søker du om helsesertifikat for sjømat
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">
+            Slik søker du om helsesertifikat i ny eksportløsning
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+      </Breadcrumbs.List>,
+    ],
+  },
+};
 
-export const MobileViewport: StoryFn<typeof Breadcrumbs> = (args) => (
-  <Breadcrumbs aria-label="Du er her:" {...args}>
-    <Breadcrumbs.Link href="#" aria-label="Tilbake til Nivå 3">
-      Nivå 3
-    </Breadcrumbs.Link>
-    <Breadcrumbs.List>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-      <Breadcrumbs.Item>
-        <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
-      </Breadcrumbs.Item>
-    </Breadcrumbs.List>
-  </Breadcrumbs>
-);
-
-MobileViewport.parameters = {
-  viewport: {
-    defaultViewport: '375px', // Large mobile default viewport
+export const MobileViewport: Story = {
+  args: {
+    'aria-label': 'Du er her:',
+    children: [
+      <Breadcrumbs.Link href="#" aria-label="Tilbake til Nivå 3">
+        Nivå 3
+      </Breadcrumbs.Link>,
+      <Breadcrumbs.List>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 1</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 2</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 3</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Nivå 4</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+      </Breadcrumbs.List>,
+    ],
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: '375px', // Large mobile default viewport
+    },
   },
 };
