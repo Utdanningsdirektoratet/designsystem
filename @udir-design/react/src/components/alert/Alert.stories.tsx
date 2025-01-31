9a2ceb776e4a5a949d341d23951cb9f6208e8e1b
Adrian Flatner <adrianrflatner@gmail.com>
2025-01-31 10:27:01 +0100

    docs: updating to using storyObj

--- @udir-design/react/src/components/alert/Alert.stories.tsx
+++ @udir-design/react/src/components/alert/Alert.stories.tsx
@@ -1,172 +1,215 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { Heading, Link, Paragraph } from '../alpha';
 
 import { Alert } from './Alert';
 
-type Story = StoryFn<typeof Alert>;
+type Story = StoryObj<typeof Alert>;
 
-const meta: Meta = {
+const meta: Meta<typeof Alert> = {
   component: Alert,
   tags: ['alpha'],
 };
 
 export default meta;
 
-export const Preview: Story = (args) => <Alert {...args}></Alert>;
-
-Preview.args = {
-  children: 'En beskjed det er viktig at brukeren ser',
-  'data-color': 'info',
+export const Preview: Story = {
+  args: {
+    'data-color': 'info',
+    children: 'En beskjed det er viktig at brukeren ser',
+  },
 };
 
-export const VariantInfo: Story = (args) => (
-  <Alert {...args} data-color="info">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Har du husket å bestille passtime?
-    </Heading>
-    <Paragraph>
-      Det er lange køer for å bestille pass om dagen, det kan være lurt å
-      bestille i god tid før du skal reise.
-    </Paragraph>
-  </Alert>
-);
+export const VariantInfo: Story = {
+  args: {
+    'data-color': 'info',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Har du husket å bestille passtime?
+        </Heading>
+        <Paragraph>
+          Det er lange køer for å bestille pass om dagen, det kan være lurt å
+          bestille i god tid før du skal reise.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const VariantSuccess: Story = (args) => (
-  <Alert {...args} data-color="success">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Gratulerer! Du kan nå starte selskapet ditt
-    </Heading>
-    <Paragraph>
-      Det ser ut til at regnestykket går i pluss og at du har det som skal til
-      for å starte selskapet ditt.
-    </Paragraph>
-  </Alert>
-);
+export const VariantSuccess: Story = {
+  args: {
+    'data-color': 'success',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Gratulerer! Du kan nå starte selskapet ditt
+        </Heading>
+        <Paragraph>
+          Det ser ut til at regnestykket går i pluss og at du har det som skal
+          til for å starte selskapet ditt.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const VariantWarning: Story = (args) => (
-  <Alert {...args} data-color="warning">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Vi har tekniske problemer
-    </Heading>
-    <Paragraph>
-      Det gjør at du kan bli avbrutt mens du fyller ut skjemaet. Vi jobber med å
-      rette problemene.
-    </Paragraph>
-  </Alert>
-);
+export const VariantWarning: Story = {
+  args: {
+    'data-color': 'warning',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Vi har tekniske problemer
+        </Heading>
+        <Paragraph>
+          Det gjør at du kan bli avbrutt mens du fyller ut skjemaet. Vi jobber
+          med å rette problemene.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const VariantDanger: Story = (args) => (
-  <Alert {...args} data-color="danger">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Det har skjedd en feil
-    </Heading>
-    <Paragraph>
-      Vi klarer ikke å hente informasjonen du ser etter akkurat nå. Prøv igjen
-      litt senere. Hvis vi fortsatt ikke klarer å vise informasjonen du trenger,
-      tar du kontakt med kundeservice på telefon 85 44 32 66.
-    </Paragraph>
-  </Alert>
-);
+export const VariantDanger: Story = {
+  args: {
+    'data-color': 'danger',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Det har skjedd en feil
+        </Heading>
+        <Paragraph>
+          Vi klarer ikke å hente informasjonen du ser etter akkurat nå. Prøv
+          igjen litt senere. Hvis vi fortsatt ikke klarer å vise informasjonen
+          du trenger, tar du kontakt med kundeservice på telefon 85 44 32 66.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const MedHeading: Story = (args) => (
-  <Alert {...args}>
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Har du husket å bestille passtime?
-    </Heading>
-    <Paragraph>
-      Det er lange køer for å bestille pass om dagen, det kan være lurt å
-      bestille i god tid om du trenger pass til sommeren.
-    </Paragraph>
-  </Alert>
-);
+export const MedHeading: Story = {
+  args: {
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Har du husket å bestille passtime?
+        </Heading>
+        <Paragraph>
+          Det er lange køer for å bestille pass om dagen, det kan være lurt å
+          bestille i god tid om du trenger pass til sommeren.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const MedKunHeading: Story = (args) => (
-  <Alert {...args} data-color="warning">
-    <Paragraph> Du har 7 dager igjen på å fullføre søknaden.</Paragraph>
-  </Alert>
-);
+export const MedKunHeading: Story = {
+  args: {
+    'data-color': 'warning',
+    children: (
+      <Paragraph> Du har 7 dager igjen på å fullføre søknaden.</Paragraph>
+    ),
+  },
+};
 
-export const MedLenke: Story = (args) => (
-  <Alert {...args} data-color="warning">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Søknadsfristen går ut om 3 dager
-    </Heading>
-    <Paragraph>
-      Fristen for å søke opptak til utdanning er 15. april.{' '}
-      <Link href="https://udir.no/">Søk nå</Link>
-    </Paragraph>
-  </Alert>
-);
+export const MedLenke: Story = {
+  args: {
+    'data-color': 'warning',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Søknadsfristen går ut om 3 dager
+        </Heading>
+        <Paragraph>
+          Fristen for å søke opptak til utdanning er 15. april.{' '}
+          <Link href="https://udir.no/">Søk nå</Link>
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const UtenAria: Story = (args) => (
-  <Alert {...args} data-color="warning">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Nedetid
-    </Heading>
-    <Paragraph>
-      I natt klokken 00:00 til 02:00 vil nettsiden være nede på grunn av
-      vedlikehold.
-    </Paragraph>
-  </Alert>
-);
+export const UtenAria: Story = {
+  args: {
+    'data-color': 'warning',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Nedetid
+        </Heading>
+        <Paragraph>
+          I natt klokken 00:00 til 02:00 vil nettsiden være nede på grunn av
+          vedlikehold.
+        </Paragraph>
+      </>
+    ),
+  },
+};
 
-export const MedAria: Story = (args) => (
-  <Alert {...args} data-color="danger" role="alert">
-    <Heading
-      level={2}
-      data-size="xs"
-      style={{
-        marginBottom: 'var(--ds-size-2)',
-      }}
-    >
-      Vi klarer ikke lagre skjemaet
-    </Heading>
-    <Paragraph>
-      Vi har mistet forbindelsen med serveren og får ikke lagret skjemaet. Vent
-      litt og prøv en gang til.
-    </Paragraph>
-  </Alert>
-);
+export const MedAria: Story = {
+  args: {
+    'data-color': 'danger',
+    role: 'alert',
+    children: (
+      <>
+        <Heading
+          level={2}
+          data-size="xs"
+          style={{
+            marginBottom: 'var(--ds-size-2)',
+          }}
+        >
+          Vi klarer ikke lagre skjemaet
+        </Heading>
+        <Paragraph>
+          Vi har mistet forbindelsen med serveren og får ikke lagret skjemaet.
+          Vent litt og prøv en gang til.
+        </Paragraph>
+      </>
+    ),
+  },
+};
