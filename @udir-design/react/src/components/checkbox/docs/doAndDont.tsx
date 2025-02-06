a0b91ddc3214374a7ff3dbc5fe4a9e890f322084
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-06 13:33:20 +0100

    docs: copy stories for checkbox from digdir

---
+++ @udir-design/react/src/components/checkbox/docs/doAndDont.tsx
@@ -0,0 +1,47 @@
+import { Checkbox, Fieldset } from '../../alpha';
+import { Do, Dont, Stack } from '../../../../.storybook/docs-components';
+
+export const CheckboxEx1 = () => {
+  return (
+    <Stack style={{ margin: 'var(--ds-size-8) 0' }}>
+      <Do description="En knapp skal kun gjøre én handling">
+        <Ex1Do />
+      </Do>
+      <Dont description="Unngå flere handlinger på samme knapp">
+        <Ex1Dont />
+      </Dont>
+    </Stack>
+  );
+};
+
+const Ex1Do = () => {
+  return (
+    <Fieldset
+      style={{
+        flexDirection: 'column',
+      }}
+    >
+      <Fieldset.Legend>
+        Skal du reise til noen av disse landene?
+      </Fieldset.Legend>
+      <Checkbox label="Kroatia" value="kroatia" />
+      <Checkbox label="Slovakia" value="slovakia" />
+      <Checkbox label="Hobsyssel" value="hobsyssel" />
+    </Fieldset>
+  );
+};
+
+const Ex1Dont = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>
+        Skal du reise til noen av disse landene?
+      </Fieldset.Legend>
+      <Stack direction="row" gap="var(--ds-size-8)">
+        <Checkbox label="Kroatia" value="kroatia" />
+        <Checkbox label="Slovakia" value="slovakia" />
+        <Checkbox label="Hobsyssel" value="hobsyssel" />
+      </Stack>
+    </Fieldset>
+  );
+};
