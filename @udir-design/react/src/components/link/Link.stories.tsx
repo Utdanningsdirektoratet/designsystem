2e413d63ea5eb7da39bb054ea916b686652df42e
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-07 10:31:49 +0100

    docs: copy docs for link from digdir

--- @udir-design/react/src/components/link/Link.stories.tsx
+++ @udir-design/react/src/components/link/Link.stories.tsx
@@ -1,17 +1,68 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Link } from './Link';
+import { Paragraph } from '../alpha';
+import { EnvelopeClosedIcon } from '@navikt/aksel-icons';
 
 const meta: Meta<typeof Link> = {
   component: Link,
   tags: ['alpha'],
+  parameters: {
+    customStyles: { padding: '2px' },
+    status: {
+      type: 'beta',
+      url: 'http://www.url.com/status',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Link>;
 
+const udirLink = 'https://udir.no/';
+
 export const Preview: Story = {
   args: {
-    href: 'https://www.udir.no/',
-    children: 'Gå til udir.no',
+    href: udirLink,
+    children: 'Gå til udirs hjemmeside',
+  },
+};
+
+export const InText: Story = {
+  render: (args) => (
+    <Paragraph>
+      Designsystemet må ses i sammenheng med{' '}
+      <Link href="https://www.udir.no/om-udir/mal-og-oppgaver/" {...args}>
+        Udirs mål og oppgaver
+      </Link>
+      .
+    </Paragraph>
+  ),
+};
+
+export const WithIcon: Story = {
+  args: {
+    href: 'mailto:designsystem@digdir.no',
+    children: [<EnvelopeClosedIcon aria-hidden />, 'Kontakt oss'],
+  },
+};
+
+export const LongLink: Story = {
+  parameters: {
+    customStyles: { width: '200px' },
+  },
+  render: (args) => (
+    <Paragraph>
+      <Link href={udirLink} {...args}>
+        Dette er en lenke som brekker over flere linjer
+      </Link>
+    </Paragraph>
+  ),
+};
+
+export const Neutral: Story = {
+  args: {
+    children: 'Gå til designsystemet',
+    href: udirLink,
+    'data-color': 'neutral',
   },
 };
