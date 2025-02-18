1adb1549462830082c902c5919c34a8bdf698a14
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-18 14:42:59 +0100

    test: add interaction test for textfield

--- @udir-design/react/src/components/textfield/Textfield.stories.tsx
+++ @udir-design/react/src/components/textfield/Textfield.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Textfield } from './Textfield';
-import { Label } from '../typography/label/Label';
+import { expect, userEvent, within } from '@storybook/test';
 
 const meta: Meta<typeof Textfield> = {
   component: Textfield,
@@ -11,10 +11,58 @@ export default meta;
 type Story = StoryObj<typeof Textfield>;
 
 export const Preview: Story = {
-  render: (args) => (
-    <>
-      <Label htmlFor="my-textfield">Label</Label>
-      <Textfield id="my-textfield" {...args} />
-    </>
-  ),
+  args: {
+    label: 'Label',
+    disabled: false,
+    readOnly: false,
+    multiline: false,
+    description: '',
+    error: '',
+    counter: 0,
+    id: 'textfield-preview',
+  },
+  play: async ({ canvasElement, step, args }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByRole('textbox', {
+      name: args.label as string,
+    });
+
+    await step('Label is rendered', async () => {
+      const labelElement = canvas.getByText(args.label as string);
+      expect(labelElement).toBeInTheDocument();
+    });
+
+    await step(
+      'Input field is rendered and associated with the label',
+      async () => {
+        expect(input).toBeInTheDocument();
+        expect(input).toHaveAttribute('id', args.id);
+      }
+    );
+
+    await step('User can focus the text field', async () => {
+      await userEvent.click(input);
+      expect(input).toHaveFocus();
+    });
+
+    await step('User can blur the text field', async () => {
+      await userEvent.tab();
+      expect(input).not.toHaveFocus();
+    });
+
+    if (!args.disabled && !args.readOnly) {
+      await step('User can type in the text field', async () => {
+        await userEvent.clear(input);
+        await userEvent.type(input, 'Hello World');
+        expect(input).toHaveValue('Hello World');
+      });
+    } else {
+      await step(
+        'Text field is disabled or read-only so it should not be editable',
+        async () => {
+          expect(input).toBeDisabled();
+        }
+      );
+    }
+  },
 };
