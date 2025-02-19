bcd4f8b5c2498c782ab2c2466dc6f43c3fd11565
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-19 13:22:29 +0100

    build: upgrade @digdir packages to 1.0.0-next.49

--- @udir-design/react/src/components/alpha.ts
+++ @udir-design/react/src/components/alpha.ts
@@ -20,7 +20,7 @@ export * from './input/Input';
 export * from './link/Link';
 export * from './list/List';
 export * from './loaders';
-export * from './modal/Modal';
+export * from './dialog/Dialog';
 export * from './pagination/Pagination';
 export * from './popover/Popover';
 export * from './radio/Radio';
