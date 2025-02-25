f11b2a6ef6c0a7efc494c7b31452462a88488a2e
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-25 15:43:22 +0100

    docs: copy docs for toggleGroup from digdir

--- @udir-design/react/src/components/toggleGroup/ToggleGroup.stories.tsx
+++ @udir-design/react/src/components/toggleGroup/ToggleGroup.stories.tsx
@@ -1,5 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { ToggleGroup } from './ToggleGroup';
+import { Button, Divider, Paragraph, ToggleGroup, Tooltip } from '../alpha';
+import {
+  AlignCenterIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
+  ArchiveIcon,
+  DocPencilIcon,
+  EnvelopeClosedIcon,
+  PaperplaneIcon,
+} from '@navikt/aksel-icons';
+import { useState } from 'react';
 
 const meta: Meta<typeof ToggleGroup> = {
   component: ToggleGroup,
@@ -12,13 +22,68 @@ type Story = StoryObj<typeof ToggleGroup>;
 export const Preview: Story = {
   args: {
     defaultValue: 'innboks',
+    children: [
+      <ToggleGroup.Item value="innboks">Innboks</ToggleGroup.Item>,
+      <ToggleGroup.Item value="utkast">Utkast</ToggleGroup.Item>,
+      <ToggleGroup.Item value="arkiv">Arkiv</ToggleGroup.Item>,
+      <ToggleGroup.Item value="sendt">Sendt</ToggleGroup.Item>,
+    ],
   },
-  render: (args) => (
-    <ToggleGroup {...args}>
-      <ToggleGroup.Item value="innboks">Innboks</ToggleGroup.Item>
-      <ToggleGroup.Item value="utkast">Utkast</ToggleGroup.Item>
-      <ToggleGroup.Item value="arkiv">Arkiv</ToggleGroup.Item>
-      <ToggleGroup.Item value="sendt">Sendt</ToggleGroup.Item>
-    </ToggleGroup>
-  ),
+};
+
+export const OnlyIcons: Story = {
+  args: {
+    defaultValue: 'option-1',
+    'data-color': 'accent',
+    children: [
+      <Tooltip content="Venstrestilt">
+        <ToggleGroup.Item value="option-1" icon>
+          <AlignLeftIcon title="AlignLeftIcon" />
+        </ToggleGroup.Item>
+      </Tooltip>,
+      <Tooltip content="Midtstilt">
+        <ToggleGroup.Item value="option-2" icon>
+          <AlignCenterIcon title="AlignCenterIcon" />
+        </ToggleGroup.Item>
+      </Tooltip>,
+      <Tooltip content="Høyrestilt">
+        <ToggleGroup.Item value="option-3" icon>
+          <AlignRightIcon title="AlignRightIcon" />
+        </ToggleGroup.Item>
+      </Tooltip>,
+    ],
+  },
+};
+
+export const Kontrollert: Story = {
+  render: function Render(args) {
+    const [value, setValue] = useState<string>('utkast');
+    return (
+      <>
+        <ToggleGroup value={value} onChange={setValue} {...args}>
+          <ToggleGroup.Item value="innboks">
+            <EnvelopeClosedIcon aria-hidden />
+            Innboks
+          </ToggleGroup.Item>
+          <ToggleGroup.Item value="utkast">
+            <DocPencilIcon aria-hidden />
+            Utkast
+          </ToggleGroup.Item>
+          <ToggleGroup.Item value="arkiv">
+            <ArchiveIcon aria-hidden />
+            Arkiv
+          </ToggleGroup.Item>
+          <ToggleGroup.Item value="sendt">
+            <PaperplaneIcon aria-hidden />
+            Sendt
+          </ToggleGroup.Item>
+        </ToggleGroup>
+        <Divider />
+        <Paragraph>Du har valgt: {value}</Paragraph>
+        <Button data-size="sm" onClick={() => setValue('arkiv')}>
+          Velg Arkiv
+        </Button>
+      </>
+    );
+  },
 };
