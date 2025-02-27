37ef1f2e7bce778eaf953d74f88e5adf5f222357
Adrian Flatner <adrianrflatner@gmail.com>
2025-02-27 14:51:30 +0100

    docs: copy loaders from digdir

--- @udir-design/react/src/components/loaders/spinner/Spinner.stories.tsx
+++ @udir-design/react/src/components/loaders/spinner/Spinner.stories.tsx
@@ -4,6 +4,14 @@ import { Spinner } from './Spinner';
 const meta: Meta<typeof Spinner> = {
   component: Spinner,
   tags: ['alpha'],
+  parameters: {
+    customStyles: {
+      display: 'grid',
+      gap: '1rem',
+      gridTemplateColumns: 'repeat(3, auto)',
+      placeItems: 'center',
+    },
+  },
 };
 
 export default meta;
@@ -13,4 +21,34 @@ export const Preview: Story = {
   args: {
     'aria-label': 'Laster...',
   },
+};
+
+export const Variants: Story = {
+  args: {
+    'aria-label': 'Henter kaffe...',
+    'data-size': 'xl',
+  },
+  render: (args) => (
+    <>
+      <Spinner {...args} data-color="neutral" />
+      <Spinner {...args} data-color="accent" />
+      <Spinner {...args} data-color="support2" />
+    </>
+  ),
+};
+
+export const Sizes: Story = {
+  args: {
+    'aria-label': 'Henter kaffe...',
+  },
+  render: (args) => (
+    <>
+      <Spinner {...args} data-size="2xs" />
+      <Spinner {...args} data-size="xs" />
+      <Spinner {...args} data-size="sm" />
+      <Spinner {...args} data-size="md" />
+      <Spinner {...args} data-size="lg" />
+      <Spinner {...args} data-size="xl" />
+    </>
+  ),
 };
