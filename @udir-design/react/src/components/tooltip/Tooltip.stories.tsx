edc3e828fef913ff0885f71a8ca5915ce3673285
Adrian Flatner <adrianrflatner@gmail.com>
2025-03-03 14:21:16 +0100

    docs: copy tooltip docs from digdir

--- @udir-design/react/src/components/tooltip/Tooltip.stories.tsx
+++ @udir-design/react/src/components/tooltip/Tooltip.stories.tsx
@@ -5,6 +5,9 @@ import { Button } from '../button/Button';
 const meta: Meta<typeof Tooltip> = {
   component: Tooltip,
   tags: ['alpha'],
+  parameters: {
+    customStyles: { margin: '2rem' },
+  },
 };
 
 export default meta;
@@ -14,5 +17,20 @@ export const Preview: Story = {
   args: {
     content: 'Tooltip tekst',
     children: <Button>Hold peker over meg</Button>,
+  },
+};
+
+export const WithString: Story = {
+  args: {
+    content: 'Tooltip text',
+    children: 'Hold peker over meg',
+  },
+};
+
+export const Placement: Story = {
+  args: {
+    content: 'Tooltip text',
+    placement: 'bottom',
+    children: 'Hold peker over meg',
   },
 };
