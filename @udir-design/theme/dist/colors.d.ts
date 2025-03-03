fc758d9217417583641776ae011ed352821ba5f0
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-03-03 15:40:52 +0100

    build(theme): check in ./dist directory to correctly bump version when only tokens have changed

---
+++ @udir-design/theme/dist/colors.d.ts
@@ -0,0 +1,10 @@
+import type { MainAndSupportColors as BaseCustomColors } from '@digdir/designsystemet-react/colors';
+
+declare module '@digdir/designsystemet-react/colors' {
+  export interface MainAndSupportColors extends BaseCustomColors {
+    accent: never;
+    support1: never;
+    support2: never;
+    support3: never;
+  }
+}
