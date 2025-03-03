a4cdefdee878868035017f926536ea0dc5a5eb63
Adrian Flatner <adrianrflatner@gmail.com>
2025-03-03 09:13:56 +0100

    docs: copy switch docs from digdir

--- @udir-design/react/src/components/switch/Switch.stories.tsx
+++ @udir-design/react/src/components/switch/Switch.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Switch } from './Switch';
 import { Card } from '@digdir/designsystemet-react';
 import { expect, within } from 'storybook/test';
+import { Fieldset } from '../alpha';
 
 const meta: Meta<typeof Switch> = {
   component: Switch,
@@ -16,6 +17,42 @@ export const Preview: Story = {
     label: 'En bryter',
   },
   render: (args, context) => <Switch {...args} id={context.id} />,
+};
+
+export const Checked: Story = {
+  ...Preview,
+  args: { ...Preview.args, checked: true },
+};
+
+export const Group: Story = {
+  render: ({ 'aria-label': a, 'aria-labelledby': b, ...args }) => (
+    <Fieldset>
+      <Fieldset.Legend>Skru av/på en eller flere innstillinger</Fieldset.Legend>
+      <Switch
+        label="Innstilling 1"
+        description="Beskrivelse av innstilling"
+        value="alt1"
+        checked
+        {...args}
+      />
+      <Switch label="Innstilling 2" value="alt2" {...args} />
+      <Switch label="Innstilling 3" value="alt3" {...args} />
+      <Switch
+        label="Innstilling 4"
+        value="alt4"
+        description="Du mangler rettigheter for denne instillingen"
+        readOnly
+        {...args}
+      />
+    </Fieldset>
+  ),
+};
+
+export const GroupEnd: Story = {
+  ...Group,
+  args: {
+    position: 'end',
+  },
 };
 
 export const SwitchInColorContext: Story = {
