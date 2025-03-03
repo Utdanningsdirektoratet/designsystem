ddade7bc706e5124276ad3001c3faaff14ee3a2e
Adrian Flatner <adrianrflatner@gmail.com>
2025-03-03 14:59:11 +0100

    test: add interaction test for tooltip

--- @udir-design/react/src/components/tooltip/Tooltip.stories.tsx
+++ @udir-design/react/src/components/tooltip/Tooltip.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Tooltip } from './Tooltip';
 import { Button } from '../button/Button';
+import { expect, userEvent, waitFor, within } from '@storybook/test';
 
 const meta: Meta<typeof Tooltip> = {
   component: Tooltip,
@@ -17,6 +18,35 @@ export const Preview: Story = {
   args: {
     content: 'Tooltip tekst',
     children: <Button>Hold peker over meg</Button>,
+  },
+  play: async ({ canvasElement, step, args }) => {
+    const canvas = within(canvasElement);
+    const trigger = canvas.getByRole('button');
+
+    await step('Tooltip is not visible initially', async () => {
+      const tooltip = canvas.queryByText(args.content);
+      expect(tooltip).not.toBeVisible();
+    });
+
+    await step('Hovering over the button displays the tooltip', async () => {
+      await userEvent.hover(trigger);
+      const tooltip = canvas.getByRole('tooltip');
+      await waitFor(() => {
+        expect(tooltip).toBeVisible();
+      });
+      await userEvent.unhover(trigger);
+      expect(tooltip).not.toBeVisible();
+    });
+
+    await step('Focus on the button displays the tooltip', async () => {
+      await userEvent.tab();
+      const tooltip = canvas.getByRole('tooltip');
+      await waitFor(() => {
+        expect(tooltip).toBeVisible();
+      });
+      await userEvent.tab();
+      expect(tooltip).not.toBeVisible();
+    });
   },
 };
 
