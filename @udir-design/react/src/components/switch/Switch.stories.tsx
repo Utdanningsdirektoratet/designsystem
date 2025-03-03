6b8df6479c601e12ac68587a2f64096745cfc954
Adrian Flatner <adrianrflatner@gmail.com>
2025-03-03 10:52:15 +0100

    test: add interaction test for switch

--- @udir-design/react/src/components/switch/Switch.stories.tsx
+++ @udir-design/react/src/components/switch/Switch.stories.tsx
@@ -3,6 +3,7 @@ import { Switch } from './Switch';
 import { Card } from '@digdir/designsystemet-react';
 import { expect, within } from 'storybook/test';
 import { Fieldset } from '../alpha';
+import { expect, userEvent, within } from '@storybook/test';
 
 const meta: Meta<typeof Switch> = {
   component: Switch,
@@ -17,15 +18,34 @@ export const Preview: Story = {
     label: 'En bryter',
   },
   render: (args, context) => <Switch {...args} id={context.id} />,
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const toggleSwitch = canvas.getByRole('switch');
+
+    await step('Switch is rendered and initially off', async () => {
+      expect(toggleSwitch).toBeInTheDocument();
+      expect(toggleSwitch).not.toBeChecked();
+    });
+
+    await step('User can toggle the switch on and off', async () => {
+      await userEvent.click(toggleSwitch);
+      expect(toggleSwitch).toBeChecked();
+      await userEvent.click(toggleSwitch);
+      expect(toggleSwitch).not.toBeChecked();
+    });
+  },
 };
 
 export const Checked: Story = {
   ...Preview,
   args: { ...Preview.args, checked: true },
+  play: () => {
+    // Do nothing
+  },
 };
 
 export const Group: Story = {
-  render: ({ 'aria-label': a, 'aria-labelledby': b, ...args }) => (
+  render: ({ 'aria-label': a, 'aria-labelledby': b, ...args }, context) => (
     <Fieldset>
       <Fieldset.Legend>Skru av/på en eller flere innstillinger</Fieldset.Legend>
       <Switch
@@ -34,15 +54,27 @@ export const Group: Story = {
         value="alt1"
         checked
         {...args}
+        id={context.id + '-1'}
       />
-      <Switch label="Innstilling 2" value="alt2" {...args} />
-      <Switch label="Innstilling 3" value="alt3" {...args} />
+      <Switch
+        label="Innstilling 2"
+        value="alt2"
+        {...args}
+        id={context.id + '-2'}
+      />
+      <Switch
+        label="Innstilling 3"
+        value="alt3"
+        {...args}
+        id={context.id + '-3'}
+      />
       <Switch
         label="Innstilling 4"
         value="alt4"
         description="Du mangler rettigheter for denne instillingen"
         readOnly
         {...args}
+        id={context.id + '-4'}
       />
     </Fieldset>
   ),
