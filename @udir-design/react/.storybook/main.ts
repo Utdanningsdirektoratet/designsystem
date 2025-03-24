6332dd3de112325ebf11d78d9c6eb0bbbcf67f75
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-03-24 11:28:14 +0100

    chore(Storybook): type hint in viteFinal config

--- @udir-design/react/.storybook/main.ts
+++ @udir-design/react/.storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import remarkGfm from 'remark-gfm';
+import { UserConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
@@ -50,7 +51,7 @@ const config: StorybookConfig = {
         */
         include: ['react/jsx-dev-runtime'],
       },
-    });
+    } satisfies UserConfig);
   },
 
   tags: {
