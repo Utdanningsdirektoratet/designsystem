2f98bea0f08d709bf5708b6bb2cc9a1c2d5f7fee
Adrian Flatner <adrianrflatner@gmail.com>
2025-04-01 12:57:23 +0200

    docs: improving docs for details

--- @udir-design/react/src/components/details/Details.stories.tsx
+++ @udir-design/react/src/components/details/Details.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
-import { Button, Card, Details, Link } from '@udir-design/react/alpha';
+import { Button, Card, Details, List } from '@udir-design/react/alpha';
 import { expect, userEvent, within } from '@storybook/test';
 
 export default {
@@ -10,9 +10,9 @@ export default {
 
 type Story = StoryObj<typeof Details>;
 
-const previewSummary = 'Hvem kan registrere seg i Frivillighetsregisteret?';
+const previewSummary = 'Kva er nasjonale prøver?';
 const previewContent =
-  'For å kunne bli registrert i Frivillighetsregisteret, må organisasjonen drive frivillig virksomhet. Det er bare foreninger, stiftelser og aksjeselskap som kan registreres. Virksomheten kan ikke dele ut midler til fysiske personer. Virksomheten må ha et styre.';
+  'Føremålet med nasjonale prøver er å gi skolane kunnskap om elevane sine grunnleggjande ferdigheiter i lesing, rekning og engelsk. Informasjonen frå prøvene skal danne grunnlag for undervegsvurdering og kvalitetsutvikling på alle nivå i skolesystemet.';
 
 export const Preview: Story = {
   args: {
@@ -85,87 +85,28 @@ export const InCardWithColor: Story = {
     return (
       <Card data-color="support2" variant="tinted">
         <Details {...args}>
-          <Details.Summary>
-            Hvordan får jeg tildelt et jegernummer?
-          </Details.Summary>
-          <Details.Content>
-            Du vil automatisk få tildelt jegernummer og bli registrert i
-            Jegerregisteret når du har bestått jegerprøven.
-          </Details.Content>
+          <Details.Summary>{previewSummary}</Details.Summary>
+          <Details.Content>{previewContent}</Details.Content>
         </Details>
         <Details>
           <Details.Summary>
-            Jeg har glemt jegernummeret mitt. Hvor finner jeg dette?
+            Korleis skal nasjonale prøver brukast?
           </Details.Summary>
           <Details.Content>
-            <p style={{ marginTop: 0 }}>
-              Du kan finne dette ved å logge inn på{' '}
-              <Link href="https://minjegerside.brreg.no/">Min side</Link>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              eu orci nisi. Nulla sed sem eget odio pellentesque venenatis vitae
-              et sem. Nunc vulputate nibh id nunc condimentum, et mattis quam
-              vehicula. Praesent gravida turpis eget tincidunt sodales. Praesent
-              ante arcu, semper at rhoncus ut, commodo ut ligula. Phasellus quis
-              nibh vitae dolor faucibus dictum et dapibus justo. Morbi
-              scelerisque sem id nisi ornare, in facilisis felis molestie.
-            </p>
-            <p>
-              Vivamus maximus eget mi ut aliquam. Nulla facilisi. Sed lobortis,
-              dui at facilisis scelerisque, tellus justo sodales enim, at luctus
-              diam turpis id diam. Sed vel magna eget nulla ornare lacinia.
-              Mauris commodo erat at dui interdum viverra. Morbi rhoncus dolor
-              in massa vehicula, aliquam dictum tortor luctus. Quisque vel
-              feugiat libero.
-            </p>
-            <p>
-              Nullam sed quam vestibulum, scelerisque nisl vel, rhoncus leo.
-              Integer eu tempor ex, in vulputate erat. Quisque nisl lectus,
-              consequat sit amet ex ut, interdum tincidunt ligula. Morbi sed
-              odio a leo bibendum hendrerit. Nullam erat nisi, convallis
-              tincidunt tempor eu, iaculis scelerisque mauris. Nulla pretium
-              ornare blandit. Proin dignissim massa risus, eget euismod enim
-              lobortis at. Donec venenatis libero sed ligula convallis
-              scelerisque. Pellentesque ut aliquet ante. Pellentesque et
-              eleifend ex. Quisque posuere convallis urna et ullamcorper. Morbi
-              eu tincidunt mauris. Ut nec diam nunc. Sed sed neque facilisis,
-              luctus libero vitae, porttitor ex. Mauris euismod vitae velit eu
-              laoreet.
-            </p>
-            <p>
-              Vivamus maximus eget mi ut aliquam. Nulla facilisi. Sed lobortis,
-              dui at facilisis scelerisque, tellus justo sodales enim, at luctus
-              diam turpis id diam. Sed vel magna eget nulla ornare lacinia.
-              Mauris commodo erat at dui interdum viverra. Morbi rhoncus dolor
-              in massa vehicula, aliquam dictum tortor luctus. Quisque vel
-              feugiat libero.
-            </p>
-            <p>
-              Nullam sed quam vestibulum, scelerisque nisl vel, rhoncus leo.
-              Integer eu tempor ex, in vulputate erat. Quisque nisl lectus,
-              consequat sit amet ex ut, interdum tincidunt ligula. Morbi sed
-              odio a leo bibendum hendrerit. Nullam erat nisi, convallis
-              tincidunt tempor eu, iaculis scelerisque mauris. Nulla pretium
-              ornare blandit. Proin dignissim massa risus, eget euismod enim
-              lobortis at. Donec venenatis libero sed ligula convallis
-              scelerisque. Pellentesque ut aliquet ante. Pellentesque et
-              eleifend ex. Quisque posuere convallis urna et ullamcorper. Morbi
-              eu tincidunt mauris. Ut nec diam nunc. Sed sed neque facilisis,
-              luctus libero vitae, porttitor ex. Mauris euismod vitae velit eu
-              laoreet.
-            </p>
-            <p style={{ marginBottom: 0 }}>
-              Nulla facilisi. Maecenas vel fringilla felis. Sed orci felis,
-              volutpat ac bibendum sit amet, sodales ac purus. Fusce nisi eros,
-              tristique sed consequat sed, scelerisque et tortor. Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit. Vestibulum
-              pellentesque vehicula orci sed scelerisque. Ut nec elementum
-              tortor. Praesent lobortis eros nec laoreet iaculis. Pellentesque
-              ex purus, vulputate non volutpat non, sodales a arcu. Phasellus
-              ornare, lorem nec aliquam venenatis, augue eros sagittis quam, at
-              sagittis tellus ante in metus.
-            </p>
+            <List.Unordered>
+              <List.Item>
+                Lærarane skal bruke resultata for å følgje opp elevane sine og i
+                arbeidet med undervegsvurdering og tilpassa opplæring.
+              </List.Item>
+              <List.Item>
+                Kommunar og skolar skal bruke resultata som grunnlag for
+                kvalitetsutvikling i opplæringa.
+              </List.Item>
+              <List.Item>
+                Forskarar kan søkje om å få utlevert resultat frå nasjonale
+                prøver til bruk i forsking.
+              </List.Item>
+            </List.Unordered>
           </Details.Content>
         </Details>
       </Card>
@@ -189,32 +130,23 @@ export const Controlled: Story = {
         <Button onClick={toggleOpen}>Toggle Details</Button>
         <br />
         <Details open={open1} onToggle={() => setOpen1(!open1)}>
-          <Details.Summary>Enkeltpersonforetak</Details.Summary>
+          <Details.Summary>Feide</Details.Summary>
           <Details.Content>
-            Skal du starte for deg selv? Enkeltpersonforetak er ofte den
-            enkleste måten å etablere bedrift på. Denne organisasjonsformen har
-            både fordeler og ulemper. Det gir deg stor grad av frihet, men kan
-            også gi betydelig risiko fordi du har personlig ansvar for
-            økonomien.
+            Feide er en nasjonal fellesløsning for sikker identifisering i
+            utdanningssektoren.
           </Details.Content>
         </Details>
         <Details open={open2} onToggle={() => setOpen2(!open2)}>
-          <Details.Summary>Aksjeselskap (AS)</Details.Summary>
+          <Details.Summary>UIDP</Details.Summary>
           <Details.Content>
-            Planlegger du å starte næringsvirksomhet alene eller sammen med
-            andre? Innebærer næringsvirksomheten en økonomisk risiko? Vil du ha
-            rettigheter som arbeidstaker og muligheten til at andre kan
-            investere i selskapet ditt? Da kan aksjeselskap være en
-            hensiktsmessig organisasjonsform.
+            UIDP er Udirs nye løsning for identitets- og tilgangskontroll.
           </Details.Content>
         </Details>
         <Details open={open3} onToggle={() => setOpen3(!open3)}>
-          <Details.Summary>Ansvarlig selskap (ANS/DA)</Details.Summary>
+          <Details.Summary>UBAS</Details.Summary>
           <Details.Content>
-            Er dere minst to personer som skal starte opp egen virksomhet?
-            Samarbeider du godt med den/de som du skal starte opp sammen med?
-            Krever virksomheten få investeringer og tar du liten økonomisk
-            risiko? Da kan du vurdere å etablere et ansvarlig selskap.
+            UBAS styrer brukertilgang til flere av våre nettjenester. UBAS vil
+            gradvis bli faset ut og erstattet av nyere påloggingsløsninger.
           </Details.Content>
         </Details>
       </>
