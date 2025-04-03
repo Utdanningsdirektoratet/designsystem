556344a04810c1bbd3a0b80127d9d74841e3182e
Adrian Flatner <adrianrflatner@gmail.com>
2025-04-03 12:22:33 +0200

    docs: improving docs for checkbox

--- @udir-design/react/src/components/checkbox/docs/doAndDont.tsx
+++ @udir-design/react/src/components/checkbox/docs/doAndDont.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Fieldset } from '@udir-design/react/alpha';
+import { Checkbox, Divider, Fieldset } from '@udir-design/react/alpha';
 import { Do, Dont, Stack } from '../../../../.storybook/docs-components';
 
 export const CheckboxEx1 = () => {
@@ -21,12 +21,10 @@ const Ex1Do = () => {
         flexDirection: 'column',
       }}
     >
-      <Fieldset.Legend>
-        Skal du reise til noen av disse landene?
-      </Fieldset.Legend>
-      <Checkbox label="Kroatia" value="kroatia" />
-      <Checkbox label="Slovakia" value="slovakia" />
-      <Checkbox label="Hobsyssel" value="hobsyssel" />
+      <Fieldset.Legend>Hva vil du vite mer om?</Fieldset.Legend>
+      <Checkbox label="Barnehage" value="barnehage" />
+      <Checkbox label="Grunnskole" value="grunnskole" />
+      <Checkbox label="Videregående" value="videregaende" />
     </Fieldset>
   );
 };
@@ -34,14 +32,97 @@ const Ex1Do = () => {
 const Ex1Dont = () => {
   return (
     <Fieldset>
-      <Fieldset.Legend>
-        Skal du reise til noen av disse landene?
-      </Fieldset.Legend>
+      <Fieldset.Legend>Hva vil du vite mer om?</Fieldset.Legend>
       <Stack direction="row" gap="var(--ds-size-8)">
-        <Checkbox label="Kroatia" value="kroatia" />
-        <Checkbox label="Slovakia" value="slovakia" />
-        <Checkbox label="Hobsyssel" value="hobsyssel" />
+        <Checkbox label="Barnehage" value="barnehage" />
+        <Checkbox label="Grunnskole" value="grunnskole" />
+        <Checkbox label="Videregående" value="videregaende" />
       </Stack>
+    </Fieldset>
+  );
+};
+
+export const CheckboxEx2 = () => {
+  return (
+    <Stack style={{ margin: 'var(--ds-size-8) 0' }}>
+      <Do description="Brukeren skal kunne lese hvert svaralternativ som en logisk respons på forklaringen over boksene.">
+        <Ex2Do />
+      </Do>
+      <Dont description="Unngå å lage svaralternativer som bryter lesbarheten.">
+        <Ex2Dont />
+      </Dont>
+    </Stack>
+  );
+};
+
+const Ex2Do = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Hva vil du jobbe med?</Fieldset.Legend>
+      <Checkbox label="Utdanning" value="utdanning" />
+      <Checkbox label="Skatt" value="Skatt" />
+      <Checkbox label="Velferd" value="Velferd" />
+    </Fieldset>
+  );
+};
+
+const Ex2Dont = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Hva vil du jobbe med?</Fieldset.Legend>
+      <Checkbox label="Innenfor utdanning" value="utdanning" />
+      <Checkbox label="Som skattearbeider" value="Skatt" />
+      <Checkbox label="Ønsket mitt har alltid vært velferd" value="Velferd" />
+    </Fieldset>
+  );
+};
+
+export const CheckboxEx3 = () => {
+  return (
+    <Stack style={{ margin: 'var(--ds-size-8) 0' }}>
+      <Do description="Bruk en konsekvent språklig stil i alle svaralternativene.">
+        <Ex3Do />
+      </Do>
+      <Dont description="Unngå å blande forskjellige språklige former.">
+        <Ex3Dont />
+      </Dont>
+    </Stack>
+  );
+};
+
+export const Ex3Do = () => {
+  return (
+    <Stack>
+      <Fieldset>
+        <Fieldset.Legend>Hva liker du best med jobben din?</Fieldset.Legend>
+        <Checkbox
+          label="Jeg liker å jobbe med selvstendige oppgaver"
+          value="selvstendige"
+        />
+        <Checkbox label="Jeg elsker møter" value="moter" />
+        <Checkbox label="Lunsjen er best" value="lunsj" />
+        <Checkbox label="Jeg liker å møte kolleger" value="kolleger" />
+      </Fieldset>
+      <Divider />
+      <Fieldset>
+        <Fieldset.Legend>Hva liker du best med jobben din?</Fieldset.Legend>
+        <Checkbox label="Selvstendige oppgaver" value="selvstendige" />
+        <Checkbox label="Møter" value="moter" />
+        <Checkbox label="Lunsj" value="lunsj" />
+        <Checkbox label="Kolleger" value="kolleger" />
+      </Fieldset>
+    </Stack>
+  );
+};
+
+const Ex3Dont = () => {
+  return (
+    <Fieldset>
+      <Fieldset.Legend>Hva liker du best med jobben din?</Fieldset.Legend>
+      <Checkbox label="Selvstendige oppgaver" value="selvstendige" />
+      <Checkbox label="Møter" value="moter" />
+      <Checkbox label="Lunsjen er best" value="lunsj" />
+      <Checkbox label="Jeg liker å møte kolleger" value="kolleger" />
     </Fieldset>
   );
 };
