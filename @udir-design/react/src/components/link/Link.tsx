6bc7b9bad2f1d817ba8bfe78600b648840e34f66
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-04-23 16:53:12 +0200

    feat(Link): use "info" color instead of "neutral" by default

--- @udir-design/react/src/components/link/Link.tsx
+++ @udir-design/react/src/components/link/Link.tsx
@@ -4,7 +4,13 @@ import {
 } from '@digdir/designsystemet-react';
 import { ForwardRefExoticComponent } from 'react';
 
-type LinkProps = Omit<DigdirLinkProps, 'data-color'>;
+type LinkProps = Omit<DigdirLinkProps, 'data-color'> & {
+  /**
+   * Set the color of the link
+   * @default info
+   **/
+  'data-color'?: 'info' | 'neutral';
+};
 
 const Link = DigdirLink as ForwardRefExoticComponent<LinkProps>;
 
