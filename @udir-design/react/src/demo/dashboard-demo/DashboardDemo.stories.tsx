c25797cd5538ee60e1f567976cb631ee02394bcf
Adrian Flatner <adrianrflatner@gmail.com>
2025-05-22 15:10:46 +0200

    docs: dashboard demo

---
+++ @udir-design/react/src/demo/dashboard-demo/DashboardDemo.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DashboardDemo } from './DashboardDemo';
+import { useState } from 'react';
+import { Size } from '@digdir/designsystemet-react';
+import { Controls } from '../controls/Controls';
+
+const meta: Meta<typeof DashboardDemo> = {
+  title: 'Demo/Dashboard Demo',
+  component: DashboardDemo,
+};
+
+export default meta;
+type Story = StoryObj<typeof DashboardDemo>;
+
+export const DashboardStory: Story = {
+  parameters: {
+    customStyles: {
+      padding: 0,
+    },
+  },
+  render: function Render(args) {
+    const [size, setSize] = useState<Size>('sm');
+    const [colorMode, setColorMode] = useState('auto');
+    return (
+      <div>
+        <Controls
+          size={size}
+          colorMode={colorMode}
+          setSize={setSize}
+          setColorMode={setColorMode}
+        />
+        <DashboardDemo
+          {...args}
+          data-size={size}
+          data-color-scheme={colorMode}
+          setColorMode={setColorMode}
+        />
+      </div>
+    );
+  },
+};
