dac816106a733d4f989073e491cd8c48ef6cdaed
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-06 14:23:51 +0200

    docs: improve fieldset for beta

--- @udir-design/react/src/components/fieldset/Fieldset.stories.tsx
+++ @udir-design/react/src/components/fieldset/Fieldset.stories.tsx
@@ -1,9 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Fieldset } from './Fieldset';
-import { Field } from '../field/Field';
-import { Input } from '../input/Input';
-import { Textarea } from '../textarea/Textarea';
-import { Label } from '@digdir/designsystemet-react';
+import { Checkbox, Radio } from '@digdir/designsystemet-react';
 
 const meta: Meta<typeof Fieldset> = {
   component: Fieldset,
@@ -15,21 +12,48 @@ type Story = StoryObj<typeof Fieldset>;
 
 export const Preview: Story = {
   render: (args, context) => (
-    <form>
-      <Fieldset {...args}>
-        <Fieldset.Legend>Skriv inn dine svar</Fieldset.Legend>
-        <Fieldset.Description>
-          Gi en kort beskrivelse i begge feltene
-        </Fieldset.Description>
-        <Field>
-          <Label>Kort beskrivelse</Label>
-          <Input id={`${context.id}-input`} />
-        </Field>
-        <Field>
-          <Label>Lang beskrivelse</Label>
-          <Textarea id={`${context.id}-textarea`} />
-        </Field>
-      </Fieldset>
-    </form>
+    <Fieldset {...args}>
+      <Fieldset.Legend>Hvilken fjordarm bor du ved?</Fieldset.Legend>
+      <Fieldset.Description>
+        Valget vil hjelpe oss å forbedre innholdet vi viser deg.
+      </Fieldset.Description>
+      <Radio
+        id={`${context.id}-radio-barsnesfjorden`}
+        label="Barsnesfjorden"
+        name="radio"
+        value="barsnesfjorden"
+      />
+      <Radio
+        id={`${context.id}-radio-eidsfjorden`}
+        label="Eidsfjorden"
+        name="radio"
+        value="eidsfjorden"
+      />
+      <Radio
+        id={`${context.id}-radio-ingen-av-de`}
+        label="Ingen av de"
+        name="radio"
+        value="ingen-av-de"
+      />
+    </Fieldset>
+  ),
+};
+
+export const WithCheckbox: Story = {
+  render: (args, context) => (
+    <Fieldset {...args}>
+      <Fieldset.Legend>Godtar du vilkårene?</Fieldset.Legend>
+      <Checkbox id={`${context.id}-checkbox`} label="Ja, jeg godtar" />
+    </Fieldset>
+  ),
+};
+
+export const LegendAsHeading: Story = {
+  render: (args) => (
+    <Fieldset {...args}>
+      <Fieldset.Legend>
+        <h1>Hvor skal du reise?</h1>
+      </Fieldset.Legend>
+    </Fieldset>
   ),
 };
