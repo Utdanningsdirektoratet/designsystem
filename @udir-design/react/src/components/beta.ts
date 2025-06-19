d29c536d6c28f12275dce4cf5a888c502c6375cd
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-19 14:53:31 +0200

    feat: promote Breadcrumbs, Link and List to beta

--- @udir-design/react/src/components/beta.ts
+++ @udir-design/react/src/components/beta.ts
@@ -2,7 +2,10 @@
  * This file exports the components that are in the beta stage of development
  */
 
+export * from './breadcrumbs/Breadcrumbs';
 export * from './button/Button';
 export * from './checkbox/Checkbox';
 export * from './details/Details';
+export * from './link/Link';
+export * from './list/List';
 export * from './radio/Radio';
