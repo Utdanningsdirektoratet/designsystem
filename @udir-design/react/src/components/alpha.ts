35d267147ea9a3a51378177d6e2a5be6ef55529e
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-06-24 14:11:11 +0200

    feat: promote Table to beta and add support for tinted headers and sticky row headers

--- @udir-design/react/src/components/alpha.ts
+++ @udir-design/react/src/components/alpha.ts
@@ -12,7 +12,6 @@ export * from './popover/Popover';
 export * from './skipLink/SkipLink';
 export * from './suggestion/Suggestion';
 export * from './switch/Switch';
-export * from './table/Table';
 export * from './tabs/Tabs';
 export * from './tag/Tag';
 export * from './textarea/Textarea';
