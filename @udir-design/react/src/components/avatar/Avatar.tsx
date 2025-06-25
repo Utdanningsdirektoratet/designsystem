ad8db27af9a6d7c059ae1f78cbb6cd653121400c
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-25 15:59:52 +0200

    fix: removing variant prop for Avatar, locking the variant as circular

--- @udir-design/react/src/components/avatar/Avatar.tsx
+++ @udir-design/react/src/components/avatar/Avatar.tsx
@@ -1,2 +1,10 @@
-import { Avatar, type AvatarProps } from '@digdir/designsystemet-react';
+import {
+  Avatar as DigdirAvatar,
+  type AvatarProps as DigdirAvatarProps,
+} from '@digdir/designsystemet-react';
+import { ForwardRefExoticComponent } from 'react';
+
+type AvatarProps = Omit<DigdirAvatarProps, 'variant'>;
+const Avatar = DigdirAvatar as ForwardRefExoticComponent<AvatarProps>;
+
 export { Avatar, AvatarProps };
