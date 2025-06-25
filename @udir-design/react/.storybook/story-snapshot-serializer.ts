fe05e3a13f0f48bd8e9cf41f3bf41663894ccda1
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-25 12:14:51 +0200

    test: remove highchart autogenerated id from snapshottests

--- @udir-design/react/.storybook/story-snapshot-serializer.ts
+++ @udir-design/react/.storybook/story-snapshot-serializer.ts
@@ -5,12 +5,14 @@ const CLASS_PATTERN = /class="[^"]+"/g;
 const TEST_ID_PATTERN = /data-testid="[^"]+"/g;
 const DS_POPOVER_ARROW_PATTERN = /(--ds-popover-arrow-[xy]): [^;]+;/g;
 const TRANSLATE_PATTERN = /(translate): [^;]+;/g;
+const HIGHCHART_ID_PATTERN = /\s*id=(["'])highcharts-[^'"]+\1/g;
 
 export default {
   serialize(val) {
     const cleaned = (val as string)
       .replace(CLASS_PATTERN, '') // remove class names
       .replace(TEST_ID_PATTERN, '') // remove data-testid="..."
+      .replace(HIGHCHART_ID_PATTERN, '') // remove highcharts id
       // ensure dynamic popover arrow position doesn't break tests
       .replace(DS_POPOVER_ARROW_PATTERN, '$1: <removed>;')
       // ensure dynamic translate doesn't break tests
