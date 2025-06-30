5b57d3f3d8158de67f87ded8e23fd886387ce0bd
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-30 14:32:11 +0200

    docs(fieldset): text improvements

--- @udir-design/react/src/components/fieldset/Fieldset.stories.tsx
+++ @udir-design/react/src/components/fieldset/Fieldset.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Fieldset } from './Fieldset';
-import { Checkbox, Radio } from '@digdir/designsystemet-react';
+import {
+  Checkbox,
+  Heading,
+  Radio,
+  Textfield,
+} from '@digdir/designsystemet-react';
 
 const meta: Meta<typeof Fieldset> = {
   component: Fieldset,
   tags: ['alpha'],
+  parameters: {
+    layout: 'centered',
+  },
 };
 
 export default meta;
@@ -48,12 +56,67 @@ export const WithCheckbox: Story = {
   ),
 };
 
-export const LegendAsHeading: Story = {
-  render: (args) => (
+export const WithTextfield: Story = {
+  render: (args, context) => (
     <Fieldset {...args}>
       <Fieldset.Legend>
-        <h1>Hvor skal du reise?</h1>
+        <Heading level={2} data-size="sm">
+          Adresse
+        </Heading>
       </Fieldset.Legend>
+      <Fieldset.Description>
+        Skriv inn adressen din for å motta post fra oss.
+      </Fieldset.Description>
+      <Textfield
+        id={`${context.id}-textfield`}
+        label="Gateadresse"
+        name={`${context.id}-textfield`}
+      />
+      <Textfield
+        id={`${context.id}-textfield`}
+        label="Postnummer"
+        name={`${context.id}-textfield`}
+        pattern="[0-9]*"
+        inputMode="numeric"
+        size={4}
+      />
+      <Textfield
+        id={`${context.id}-textfield2`}
+        label="Poststed"
+        name={`${context.id}-textfield2`}
+        size={25}
+      />
+    </Fieldset>
+  ),
+};
+
+export const LegendAsHeading: Story = {
+  render: (args, context) => (
+    <Fieldset {...args}>
+      <Fieldset.Legend>
+        <Heading level={2} data-size="sm">
+          Hvilke tjenester bruker du?
+        </Heading>
+      </Fieldset.Legend>
+      <Fieldset.Description>Påloggingssystemer hos Udir</Fieldset.Description>
+      <Checkbox
+        id={`${context.id}-feide`}
+        label="Feide"
+        name={`${context.id}-feide`}
+        value="feide"
+      />
+      <Checkbox
+        id={`${context.id}-UIDP`}
+        label="UIDP"
+        name={`${context.id}-UIDP`}
+        value="UIDP"
+      />
+      <Checkbox
+        id={`${context.id}-UBAS`}
+        label="UBAS"
+        name={`${context.id}-UBAS`}
+        value="UBAS"
+      />
     </Fieldset>
   ),
 };
