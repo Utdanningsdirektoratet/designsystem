8bd82cafa93928f9dedf49d6c0feee03e2112943
Adrian Flatner <adrianrflatner@gmail.com>
2025-06-30 14:30:14 +0200

    fix: removing data-color from Fieldset

--- @udir-design/react/src/components/fieldset/Fieldset.tsx
+++ @udir-design/react/src/components/fieldset/Fieldset.tsx
@@ -1,2 +1,22 @@
-import { Fieldset, type FieldsetProps } from '@digdir/designsystemet-react';
-export { Fieldset, FieldsetProps };
+import {
+  Fieldset as DigdirFieldset,
+  type FieldsetProps as DigdirFieldsetProps,
+  FieldsetDescription,
+  type FieldsetDescriptionProps,
+  FieldsetLegend,
+  type FieldsetLegendProps,
+} from '@digdir/designsystemet-react';
+import { ForwardRefExoticComponent } from 'react';
+
+type FieldsetProps = Omit<DigdirFieldsetProps, 'data-color'>;
+const Fieldset = DigdirFieldset as ForwardRefExoticComponent<FieldsetProps> &
+  Pick<typeof DigdirFieldset, 'Legend' | 'Description'>;
+
+export {
+  Fieldset,
+  FieldsetProps,
+  FieldsetLegend,
+  FieldsetLegendProps,
+  FieldsetDescription,
+  FieldsetDescriptionProps,
+};
