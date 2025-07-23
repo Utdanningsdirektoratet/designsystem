c4ac6e6c90fb471117cd0d653d87c3abdec92eb1
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-07-23 12:50:02 +0200

    docs: promote to beta with more detailed examples

--- @udir-design/react/src/components/loaders/skeleton/Skeleton.stories.tsx
+++ @udir-design/react/src/components/loaders/skeleton/Skeleton.stories.tsx
@@ -1,11 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Button, Skeleton } from '../../beta';
+import {
+  ArrowsUpDownIcon,
+  FilterIcon,
+  PlusCircleIcon,
+} from '@navikt/aksel-icons';
+import { Button, Card } from '../../beta';
+import { Skeleton } from './Skeleton';
 import { Heading, Paragraph } from '../../alpha';
+import { Avatar } from '@digdir/designsystemet-react';
 
 const meta: Meta<typeof Skeleton> = {
   component: Skeleton,
-  tags: ['alpha'],
+  tags: ['beta'],
   parameters: {
+    layout: 'centered',
     a11y: {
       config: {
         // Disable a11y empty heading rule as we intentionally set aria-hidden="true" on the Skeleton component inside Headings
@@ -48,31 +56,160 @@ export const Components: Story = {
   },
 };
 
+const StudentData = [
+  {
+    id: 0,
+    name: 'Rita Nordmann',
+    birthday: '12.09.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+  {
+    id: 1,
+    name: 'Ola Nordmann',
+    birthday: '11.03.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+  {
+    id: 2,
+    name: 'Kari Nordmann',
+    birthday: '04.12.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+  {
+    id: 3,
+    name: 'Juliane Nordmann',
+    birthday: '24.04.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+  {
+    id: 4,
+    name: 'Mateo Nordmann',
+    birthday: '01.05.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+  {
+    id: 5,
+    name: 'Kai Nordmann',
+    birthday: '17.05.2010',
+    class: '5.-7. trinn',
+    teacher: 'Nanna Arnestad',
+  },
+];
+
+export const Cards: Story = {
+  render: (args) => {
+    return (
+      <div
+        style={{
+          margin: 'var(--ds-size-12)',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 'var(--ds-size-6)',
+        }}
+      >
+        <Heading level={3} data-size="lg">
+          Elever
+        </Heading>
+        <div style={{ display: 'flex', justifyContent: 'end' }}>
+          <Button variant="tertiary">
+            <PlusCircleIcon aria-hidden /> Legg til elev
+          </Button>
+          <Button variant="tertiary">
+            <ArrowsUpDownIcon aria-hidden /> Sorter
+          </Button>
+          <Button variant="tertiary">
+            <FilterIcon aria-hidden /> Filter
+          </Button>
+        </div>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gap: 'var(--ds-size-6)',
+          }}
+        >
+          {StudentData.map((student) => (
+            <Card key={student.id}>
+              <Card.Block>
+                <Skeleton variant="text">{student.name}</Skeleton>
+              </Card.Block>
+              <Card.Block style={{ display: 'flex', flexDirection: 'column' }}>
+                <Skeleton variant="text">
+                  Fødselsdato: {student.birthday}
+                </Skeleton>
+                <Skeleton variant="text">Trinn: {student.class}</Skeleton>
+                <Skeleton variant="text">Lærer: {student.teacher}</Skeleton>
+                <Skeleton variant="text">Vurdert: 23.04.2023</Skeleton>
+              </Card.Block>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  },
+};
+
+const schoolImage =
+  'https://images.unsplash.com/photo-1516383607781-913a19294fd1?q=80&w=2374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
 export const UsageExample: Story = {
   render: (args) => {
     return (
       <div
         style={{
-          maxWidth: 400,
+          display: 'flex',
+          gap: 'var(--ds-size-12)',
+          margin: 'var(--ds-size-12)',
         }}
       >
-        <Skeleton height="150px" {...args} />
-        <div
-          style={{
-            display: 'flex',
-            gap: '10px',
-            alignItems: 'center',
-            padding: '5px 0 5px 0',
-          }}
-        >
-          <Skeleton variant="circle" width="30px" height="30px" {...args} />
+        <div style={{ flex: '1 1 200px' }}>
+          <Skeleton height="200px" {...args} />
           <Heading>
-            <Skeleton variant="text" {...args}>
-              En medium tittel
-            </Skeleton>
+            <Skeleton variant="text" width="15" />
           </Heading>
+          <div
+            style={{
+              display: 'flex',
+              gap: '10px',
+              alignItems: 'center',
+              padding: '5px 0 5px 0',
+            }}
+          >
+            <Skeleton variant="circle" width="40px" height="40px" {...args} />
+            <Skeleton variant="text" {...args}>
+              Forfatter| sist endret
+            </Skeleton>
+          </div>
+          <Skeleton variant="text" width="200" />
         </div>
-        <Skeleton variant="text" width="140" />
+        <div style={{ flex: '1 1 200px' }}>
+          <img
+            src={schoolImage}
+            alt="Bøker på et bord"
+            style={{
+              width: '100%',
+              height: '200px',
+              objectFit: 'cover',
+              borderRadius: 10,
+            }}
+          />
+          <Heading>Undersøke</Heading>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <Avatar data-size="xs" initials="KN" aria-label={'Kari Nordmann'} />
+            <Paragraph>Kari Nordmann | 27.06.2025</Paragraph>
+          </div>
+          <Paragraph>
+            Barnehagen er pliktig til å gjennomføre undersøkelser som med
+            rimelighet kan forventes. Barnehagen må innhente nok informasjon til
+            å avdekke hva som er problemet og hva som gjør at barnet ikke har
+            det trygt og godt.
+          </Paragraph>
+        </div>
       </div>
     );
   },
@@ -98,19 +235,28 @@ export const Text: Story = {
     variant: 'text',
   },
   render: (args) => (
-    <div style={{ display: 'flex', gap: '20px', maxWidth: 300 }}>
+    <div
+      style={{
+        display: 'flex',
+        gap: 'var(--ds-size-12)',
+        margin: 'var(--ds-size-12)',
+      }}
+    >
       <div style={{ flex: '1 1 200px' }}>
-        <Heading>En tittel</Heading>
+        <Heading>Den kjekkaste leiken</Heading>
         <Paragraph data-size="sm">
-          Her er en paragraf som går over flere linjer
+          Det er verken dyrt eller komplisert å leggje til rette for den leiken
+          barna liker best. Når viser barna mest trivsel i uteleiken? Det ønskte
+          forskarar i forskingsprosjektet «EnCompetence» å finne ut, og dei
+          ville spesielt sjå på situasjonar med fysisk aktivitet.
         </Paragraph>
       </div>
       <div style={{ flex: '1 1 200px' }}>
         <Heading>
-          <Skeleton {...args}>En tittel</Skeleton>
+          <Skeleton {...args}>Den kjekkaste leiken</Skeleton>
         </Heading>
         <Paragraph data-size="sm">
-          <Skeleton width={44} {...args} />
+          <Skeleton width={262} {...args} />
         </Paragraph>
       </div>
     </div>
