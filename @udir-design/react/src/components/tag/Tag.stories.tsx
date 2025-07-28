636ed5f78c295c7163de18b626456ad7d6eedb7a
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-07-28 10:16:53 +0200

    docs: copy docs from digdir

--- @udir-design/react/src/components/tag/Tag.stories.tsx
+++ @udir-design/react/src/components/tag/Tag.stories.tsx
@@ -1,9 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Tag } from './Tag';
+import { Tag, TagProps } from './Tag';
 
 const meta: Meta<typeof Tag> = {
   component: Tag,
-  tags: ['alpha'],
+  parameters: {
+    customStyles: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      gap: 'var(--ds-size-4)',
+    },
+  },
+  tags: ['beta'],
 };
 
 export default meta;
@@ -11,6 +21,33 @@ type Story = StoryObj<typeof Tag>;
 
 export const Preview: Story = {
   args: {
-    children: 'Ny',
+    children: 'New',
   },
+};
+
+const sizes: TagProps['data-size'][] = ['sm', 'md', 'lg'];
+export const Sizes: Story = {
+  render: (args) => (
+    <>
+      {sizes.map((size) => (
+        <Tag key={size} data-size={size} {...args}>
+          {size}
+        </Tag>
+      ))}
+    </>
+  ),
+};
+
+const colorVariants = ['neutral', 'success', 'warning', 'danger', 'info'];
+
+export const Colors: Story = {
+  render: (args) => (
+    <>
+      {colorVariants.map((color) => (
+        <Tag key={color} data-color={color as TagProps['data-color']} {...args}>
+          {color}
+        </Tag>
+      ))}
+    </>
+  ),
 };
