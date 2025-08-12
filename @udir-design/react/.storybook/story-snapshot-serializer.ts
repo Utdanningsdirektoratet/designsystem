8d56622592d665dd42b9f71012143a05722456b2
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-08-12 12:45:00 +0200

    test: remove highcharts nodes from snapshot

--- @udir-design/react/.storybook/story-snapshot-serializer.ts
+++ @udir-design/react/.storybook/story-snapshot-serializer.ts
@@ -1,5 +1,9 @@
 import { SnapshotSerializer } from 'vitest';
 import diffableHtml from 'diffable-html';
+import { fromHtml } from 'hast-util-from-html';
+import { toHtml } from 'hast-util-to-html';
+import { isElement } from 'hast-util-is-element';
+import { remove } from 'unist-util-remove';
 
 const CLASS_PATTERN = /class="[^"]+"/g;
 const TEST_ID_PATTERN = /data-testid="[^"]+"/g;
@@ -17,7 +21,19 @@ export default {
       .replace(DS_POPOVER_ARROW_PATTERN, '$1: <removed>;')
       // ensure dynamic translate doesn't break tests
       .replace(TRANSLATE_PATTERN, '$1: <removed>;');
-    return diffableHtml(cleaned).trim();
+
+    const tree = fromHtml(cleaned, { fragment: true });
+    // Remove Highcharts nodes, since they are subtly different on each render
+    remove(
+      tree,
+      (node) =>
+        isElement(node) && node.properties.dataHighchartsChart !== undefined,
+    );
+    const processed = toHtml(tree, {
+      characterReferences: { useNamedReferences: true },
+    });
+
+    return diffableHtml(processed).trim();
   },
   /**
    * Should this serializer be used?
