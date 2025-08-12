7abd0fa6289071fa94132da9e0f6c7f53c23b3f1
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-08-12 12:28:32 +0200

    docs: export from beta-file

--- @udir-design/react/src/components/alpha.ts
+++ @udir-design/react/src/components/alpha.ts
@@ -4,7 +4,6 @@
 
 export * from './badge/Badge';
 export * from './dropdown/Dropdown';
-export * from './errorSummary/ErrorSummary';
 export * from './popover/Popover';
 export * from './skipLink/SkipLink';
 export * from './suggestion/Suggestion';
