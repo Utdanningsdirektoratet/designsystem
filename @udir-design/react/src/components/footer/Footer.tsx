d1e01612f834d13ff81d6a36c154900b36213b16
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-08-14 15:33:54 +0200

    feat: new component footer

---
+++ @udir-design/react/src/components/footer/Footer.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, HTMLAttributes } from 'react';
+import cl from 'clsx/lite';
+import logo from '../../../public/logo.svg';
+import { Link } from '../beta';
+
+export type FooterProps = HTMLAttributes<HTMLDivElement>;
+
+export const Footer = forwardRef<HTMLDivElement, FooterProps>(function Footer(
+  { children, className, ...rest },
+  ref,
+) {
+  return (
+    <footer className={cl(`ds-footer`, className)} ref={ref} {...rest}>
+      {children}
+      <Link href="https://udir.no">
+        <img
+          src={logo}
+          className="logo"
+          alt="Logo for utdanningsdirektoratet"
+        />
+      </Link>
+    </footer>
+  );
+});
