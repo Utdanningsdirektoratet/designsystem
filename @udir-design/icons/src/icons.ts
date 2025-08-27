6203791cdd6c0acf964237308295ddc5308cb22c
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-08-27 15:13:39 +0200

    feat: add icon library

---
+++ @udir-design/icons/src/icons.ts
@@ -0,0 +1,7 @@
+import {
+  AirplaneIcon,
+  AirplaneFillIcon,
+  AlignBottomIcon,
+  AlignBottomFillIcon,
+} from '@navikt/aksel-icons';
+export { AirplaneIcon, AirplaneFillIcon, AlignBottomIcon, AlignBottomFillIcon };
