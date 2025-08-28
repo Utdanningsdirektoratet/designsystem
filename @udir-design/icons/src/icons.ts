7d87acce8ce587c342678da6f72f2caddacfa0ab
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-08-28 10:28:14 +0200

    feat: bedre?

--- @udir-design/icons/src/icons.ts
+++
@@ -1,7 +0,0 @@
-import {
-  AirplaneIcon,
-  AirplaneFillIcon,
-  AlignBottomIcon,
-  AlignBottomFillIcon,
-} from '@navikt/aksel-icons';
-export { AirplaneIcon, AirplaneFillIcon, AlignBottomIcon, AlignBottomFillIcon };
