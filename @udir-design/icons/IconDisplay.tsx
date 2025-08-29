b3cafc89b5bc80b3279728a062d9affe599989e7
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-08-29 15:28:20 +0200

    docs: yeehaw

--- @udir-design/icons/IconDisplay.tsx
+++ @udir-design/icons/IconDisplay.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import * as Icons from './src/index';
+import { Search } from '@udir-design/react/beta';
+import { ToggleGroup } from '@udir-design/react/alpha';
 
 export const IconDisplay = () => {
   return (
     <div>
       <h1>Icon Display Component</h1>
+      <form onSubmit={() => {}}>
+        <Search>
+          <Search.Input aria-label="Søk" />
+          <Search.Clear />
+          <Search.Button />
+        </Search>
+      </form>
+      <ToggleGroup defaultValue="innboks" onChange={() => {}}>
+        <ToggleGroup.Item value="innboks">Outline</ToggleGroup.Item>
+        <ToggleGroup.Item value="utkast">Fill</ToggleGroup.Item>
+      </ToggleGroup>
       {Object.entries(Icons).map(([key, Value]) => (
         <React.Fragment key={key}>
           <Value fontSize="3rem" aria-hidden title={key} />
