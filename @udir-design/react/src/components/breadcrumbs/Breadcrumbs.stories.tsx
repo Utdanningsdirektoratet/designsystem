0f2e15e9c8d25672bb42c87ba7d09263cbfb3a34
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-09-03 16:07:56 +0200

    docs: redesign storybook tags

--- @udir-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
+++ @udir-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
@@ -4,7 +4,7 @@ import { expect, within } from 'storybook/test';
 
 export default {
   component: Breadcrumbs,
-  tags: ['beta'],
+  tags: ['beta', 'udir'],
 } as Meta<typeof Breadcrumbs>;
 
 type Story = StoryObj<typeof Breadcrumbs>;
