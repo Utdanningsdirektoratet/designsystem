be02b7d5b94f4c7ff01d6a16b08626e4febb03bc
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-09-22 11:24:37 +0200

    docs: update docs for typography

--- @udir-design/react/src/components/typography/label/Label.tsx
+++ @udir-design/react/src/components/typography/label/Label.tsx
@@ -1,2 +1,11 @@
-import { Label, type LabelProps } from '@digdir/designsystemet-react';
+import {
+  Label as DigdirLabel,
+  type LabelProps as DigdirLabelProps,
+} from '@digdir/designsystemet-react';
+import { ForwardRefExoticComponent } from 'react';
+
+type LabelProps = Omit<DigdirLabelProps, 'data-color'>;
+
+const Label = DigdirLabel as ForwardRefExoticComponent<LabelProps>;
+
 export { Label, LabelProps };
