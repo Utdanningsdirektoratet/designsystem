bfbd36e9983907a3cf3ed8a934d76559b13b1de3
Adrian Flatner <adrianrflatner@gmail.com>
2025-10-09 12:35:05 +0200

    fix(HeaderNavigation, Footer): update forwardRef types to match rendered elements
    > HTMLDivElement -> HTMLElement

--- @udir-design/react/src/components/footer/Footer.tsx
+++ @udir-design/react/src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import logoDark from '../../../assets/img/udir-main-logo-dark-mode.svg';
 import logo from '../../../assets/img/udir-main-logo.svg';
 import './footer.css';
 
-export type FooterProps = HTMLAttributes<HTMLDivElement> & {
+export type FooterProps = HTMLAttributes<HTMLElement> & {
   /**
    * The maximum width of the footer content.
    * Can be any valid CSS width value, e.g. `1280px`, `100%`, etc.
@@ -14,7 +14,7 @@ export type FooterProps = HTMLAttributes<HTMLDivElement> & {
   maxWidth?: string;
 };
 
-export const Footer = forwardRef<HTMLDivElement, FooterProps>(function Footer(
+export const Footer = forwardRef<HTMLElement, FooterProps>(function Footer(
   { children, className, maxWidth = '80rem', ...rest },
   ref,
 ) {
--- @udir-design/react/src/components/header/headerNavigation/HeaderNavigation.tsx
+++ @udir-design/react/src/components/header/headerNavigation/HeaderNavigation.tsx
@@ -1,20 +1,19 @@
 import cl from 'clsx/lite';
 import { HTMLAttributes, forwardRef } from 'react';
 
-export type HeaderNavigationProps = HTMLAttributes<HTMLDivElement>;
+export type HeaderNavigationProps = HTMLAttributes<HTMLElement>;
 
-export const HeaderNavigation = forwardRef<
-  HTMLDivElement,
-  HeaderNavigationProps
->(function HeaderNavigation({ children, className, ...rest }, ref) {
-  return (
-    <nav
-      aria-label="header-navigation"
-      className={cl('uds-header__navigation', className)}
-      ref={ref}
-      {...rest}
-    >
-      <ul>{children}</ul>
-    </nav>
-  );
-});
+export const HeaderNavigation = forwardRef<HTMLElement, HeaderNavigationProps>(
+  function HeaderNavigation({ children, className, ...rest }, ref) {
+    return (
+      <nav
+        aria-label="header-navigation"
+        className={cl('uds-header__navigation', className)}
+        ref={ref}
+        {...rest}
+      >
+        <ul>{children}</ul>
+      </nav>
+    );
+  },
+);
