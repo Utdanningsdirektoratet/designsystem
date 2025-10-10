5bc22beb5b9fec4117b7888efe23b2f7667ad211
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-10-10 18:30:48 +0200

    build: eslint config to disallow all imports from barrel files

--- @udir-design/react/eslint.config.js
+++ @udir-design/react/eslint.config.js
@@ -4,6 +4,22 @@ import baseConfig from '../../eslint.config.js';
 import storybook from 'eslint-plugin-storybook';
 import nxEslintPlugin from '@nx/eslint-plugin';
 
+const commonRestrictedImports = [
+  {
+    group: ['@udir-design/react', '@udir-design/react/*'],
+    message:
+      'Do not import from @udir-design/react module, use relative paths instead.',
+  },
+  {
+    group: ['**/dist/*'],
+    message: 'Do not import from the dist directory.',
+  },
+  {
+    group: ['**/node_modules/*'],
+    message: 'Do not import from the node_modules directory.',
+  },
+];
+
 export default defineConfig(
   baseConfig,
   nxEslintPlugin.configs['flat/react'],
@@ -22,13 +38,7 @@ export default defineConfig(
       'no-restricted-imports': [
         'error',
         {
-          patterns: [
-            {
-              group: ['@udir-design/react', '@udir-design/react/*'],
-              message:
-                'Do not import from @udir-design/react module, use relative paths instead.',
-            },
-          ],
+          patterns: commonRestrictedImports,
         },
       ],
     },
@@ -42,20 +52,31 @@ export default defineConfig(
     },
   },
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['src/alpha.ts', 'src/beta.ts', 'src/stable.ts'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            ...commonRestrictedImports,
+            {
+              regex:
+                '^((src|\\.{1,2})\\/)((\\.{1,2}\\/)|\\w+\\/)*(alpha|beta|stable)',
+              // group: ['**/alpha', '**/beta', '**/stable'],
+              message: 'Do not import from barrel files.',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
     files: ['**/*.js', '**/*.jsx'],
     rules: {},
   },
   {
     // Storybook & docs-specific overrides
     files: ['**/*.stories.{ts,tsx}', '**/{.storybook,demo,docs}/**/*.{ts,tsx}'],
-    rules: {
-      'no-restricted-imports': 'off',
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          allowCircularSelfDependency: true,
-        },
-      ],
-    },
   },
 );
