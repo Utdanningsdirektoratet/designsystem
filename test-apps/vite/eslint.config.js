7e7122902580c1bae82bcf2d5162cd38c2fcccb2
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-10-14 09:57:07 +0200

    build: eslint config for import sorting

--- test-apps/vite/eslint.config.js
+++ test-apps/vite/eslint.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'eslint/config';
 import nxEslintPlugin from '@nx/eslint-plugin';
-// eslint-disable-next-line @nx/enforce-module-boundaries
+import { defineConfig } from 'eslint/config';
 import baseConfig from '../../eslint.config.js';
 
-export default defineConfig(baseConfig, nxEslintPlugin.configs['flat/react'], {
+export default defineConfig(nxEslintPlugin.configs['flat/react'], baseConfig, {
   files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
   // Override or add rules here
   rules: {},
