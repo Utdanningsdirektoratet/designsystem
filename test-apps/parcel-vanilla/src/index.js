44f6e6d184c96ead8afc304c2f19bcb1fc92c1a4
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-10-22 12:12:12 +0200

    feat(Icons): allow referencing icon from another svg, useful when not using React
    E.g.
    ```html
    <svg aria-hidden>
      <use href="path/to/svg#root" />
    </svg>
    ```
    
    See updated icon library README for full details

--- test-apps/parcel-vanilla/src/index.js
+++ test-apps/parcel-vanilla/src/index.js
@@ -1,3 +1,4 @@
 import '@udir-design/theme/dist/index.css';
 import '@digdir/designsystemet-css/dist/src/index.css';
 import '@udir-design/css/dist/components.css';
+import '@udir-design/icons/dist/style.css';
