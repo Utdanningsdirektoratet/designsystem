d9504eb588ac26717c6e939cb48ef2014cb03c64
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-10-24 13:58:27 +0200

    docs: show more ways to use icons (css and vanilla js)

--- test-apps/parcel-vanilla/src/index.js
+++ test-apps/parcel-vanilla/src/index.js
@@ -2,3 +2,19 @@ import '@udir-design/theme/dist/index.css';
 import '@digdir/designsystemet-css/dist/src/index.css';
 import '@udir-design/css/dist/components.css';
 import '@udir-design/icons/dist/style.css';
+
+const ArrowRightUrl = new URL(
+  'npm:@udir-design/icons/dist/svg/ArrowRight.svg',
+  import.meta.url,
+);
+
+const container = document.getElementById('app');
+if (container) {
+  container.innerHTML += `
+<a class="ds-link" href="#">
+  <svg aria-hidden>
+    <use href="${ArrowRightUrl}#root"/>
+  </svg
+  ><span>Lenke lagt til dynamisk med JavaScript</span>
+</a>`;
+}
