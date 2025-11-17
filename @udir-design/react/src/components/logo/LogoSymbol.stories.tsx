a7f440b35b49c8be06630a69e2913b6b6ae943c9
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2025-11-17 15:22:45 +0100

    docs: add docs for new components Logo and LogoSymbol

---
+++ @udir-design/react/src/components/logo/LogoSymbol.stories.tsx
@@ -0,0 +1,16 @@
+import preview from '.storybook/preview';
+import { LogoSymbol } from './LogoSymbol';
+
+const meta = preview.meta({
+  component: LogoSymbol,
+  tags: ['alpha', 'udir', '!autodocs'],
+  parameters: {
+    componentOrigin: {
+      originator: 'self',
+    },
+  },
+});
+
+export const Preview = meta.story({
+  render: (args) => <LogoSymbol {...args} />,
+});
