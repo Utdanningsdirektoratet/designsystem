4b7f48b71d265ec7ce825b3a3aee64a78fecbfbf
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2025-12-03 13:06:34 +0100

    docs(formatReactSource): extract render function when formatting
    Before this change, this story
    ```jsx
    export const SomeStory = meta.story({
      parameters: {
        docs: {
          type: 'code',
          transform: formatReactSource,
        }
      },
      render: () => <SomeComponent />
    })
    ```
    
    would render in the code preview as
    ```jsx
    meta.story({
      parameters: {
        docs: {
          type: 'code',
          transform: formatReactSource,
        }
      },
      render: () => <SomeComponent />
    })
    ```
    
    ...but after this change it renders as:
    ```jsx
    const SomeStory = () => <SomeComponent />
    ```

--- @udir-design/react/.storybook/utils/sourceTransformers.ts
+++ @udir-design/react/.storybook/utils/sourceTransformers.ts
@@ -1,9 +1,11 @@
 import type { DocsTypes } from '@storybook/addon-docs';
+import camelcase from 'camelcase';
 import type { Plugin } from 'prettier';
 import * as EstreePlugin from 'prettier/plugins/estree';
 import * as HtmlPlugin from 'prettier/plugins/html';
 import * as TypescriptPlugin from 'prettier/plugins/typescript';
 import { format as prettierFormat } from 'prettier/standalone';
+import type { StoryContext } from 'storybook/internal/types';
 import type { RequiredDeep } from 'type-fest';
 
 type SourceTransformer =
@@ -13,8 +15,36 @@ type SourceTransformer =
  * Use this as parameters.docs.source.transform as needed to better format React code.
  * Not enabled by default to avoid unnecessary performance hit.
  */
-export const formatReactSource: SourceTransformer = async (src) => {
-  return formatWithPrettier('typescript', src);
+export const formatReactSource: SourceTransformer = async (
+  src,
+  ctx: StoryContext,
+) => {
+  const extractedRenderMatches = src.match(
+    /(^ +)(render:)( (?:.|\n)+)(^\1[})])/m,
+  );
+  const fullMatch = extractedRenderMatches?.[0];
+  const endingMatch = extractedRenderMatches?.[4];
+
+  let srcToFormat = src;
+  if (fullMatch && endingMatch) {
+    const matchLines = fullMatch.split('\n');
+    // The ending match might be later than the end of the render function,
+    // e.g. if a multiline object follows the render function.
+    // We need to find the location of the first match and cut the code example there.
+    const firstEndingMatchLocation = matchLines.findIndex((line) =>
+      line.startsWith(endingMatch),
+    );
+    const extractedRender = matchLines
+      .slice(0, firstEndingMatchLocation + 1)
+      .join('\n');
+    const storyNamePascal = camelcase(ctx.name, { pascalCase: true });
+    srcToFormat = extractedRender.replace(
+      'render:',
+      `const ${storyNamePascal} =`,
+    );
+  }
+
+  return formatWithPrettier('typescript', srcToFormat);
 };
 
 /**
