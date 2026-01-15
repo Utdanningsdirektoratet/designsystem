9a0defb7a3f932f5383620690fce21fd69eccb9d
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-01-15 09:20:35 +0100

    fix(Tabs): limit colors to neutral and accent for Tabs

--- @udir-design/react/src/components/tabs/Tabs.tsx
+++ @udir-design/react/src/components/tabs/Tabs.tsx
@@ -1,14 +1,31 @@
 import {
-  Tabs,
+  Tabs as DigdirTabs,
   TabsList,
   type TabsListProps,
   TabsPanel,
   type TabsPanelProps,
-  type TabsProps,
+  type TabsProps as DigdirTabsProps,
   TabsTab,
   type TabsTabProps,
 } from '@digdir/designsystemet-react';
 import './tabs.css';
+import type {
+  ComponentRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from 'react';
+
+type TabsProps = Omit<DigdirTabsProps, 'data-color'> & {
+  /**
+   * Change the color scheme of the button
+   */
+  'data-color'?: 'neutral' | 'accent';
+};
+
+const Tabs = DigdirTabs as ForwardRefExoticComponent<
+  TabsProps & RefAttributes<ComponentRef<typeof DigdirTabs>>
+> &
+  Pick<typeof DigdirTabs, 'List' | 'Tab' | 'Panel'>;
 
 // For some reason this fixes "ComponentSubcomponent" -> "Component.Subcomponent" in Storybook code snippets
 Tabs.displayName = 'Tabs';
