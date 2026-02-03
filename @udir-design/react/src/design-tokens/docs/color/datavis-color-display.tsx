4d11c00b7c81d58d9f730db6af32b40a8368df42
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-02-03 10:05:22 +0100

    docs: datavis color display

---
+++ @udir-design/react/src/design-tokens/docs/color/datavis-color-display.tsx
@@ -0,0 +1,197 @@
+import { Card } from 'src/components/card/Card';
+import { Heading } from 'src/components/typography/heading/Heading';
+import { Paragraph } from 'src/components/typography/paragraph/Paragraph';
+import {
+  datavisColors,
+  datavisSeqDivergentColors,
+  datavisSeqMonoColors,
+} from './colors';
+
+export const DatavisColorDisplay = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        gap: 'var(--ds-size-1)',
+        flexWrap: 'wrap',
+      }}
+    >
+      {datavisColors.map((color) => (
+        <Card
+          style={{
+            background: color.value,
+            flexGrow: `calc(10 - (${color.number} * 2))`,
+            color: 'var(--ds-color-neutral-base-contrast-default)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+            minHeight: '5rem',
+            borderRadius: 'var(--ds-border-radius-md)',
+          }}
+        >
+          {color.number}
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export const DatavisValueColorDisplay = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        gap: 'var(--ds-size-3)',
+        flexWrap: 'wrap',
+        marginBlockEnd: 'var(--ds-size-8)',
+      }}
+    >
+      {datavisColors.map((color) => (
+        <div
+          style={{
+            background: color.value,
+            color: '#fff',
+            padding: 'var(--ds-size-3) var(--ds-size-3)',
+            display: 'flex',
+            width: 'var(--ds-size-22)',
+            minHeight: '7rem',
+            justifySelf: 'stretch',
+            borderRadius: 'var(--ds-border-radius-md)',
+          }}
+        >
+          <Paragraph data-size="xs">{color.value}</Paragraph>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const DatavisSeqColorDisplay = () => {
+  return (
+    <div style={{ marginBlockEnd: 'var(--ds-size-8)' }}>
+      <Heading
+        data-size="sm"
+        level={3}
+        style={{ marginBlockEnd: 'var(--ds-size-4)' }}
+      >
+        Alternativ 1
+      </Heading>
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          gap: 'var(--ds-size-3)',
+          flexWrap: 'wrap',
+        }}
+      >
+        {datavisSeqMonoColors.map((color) => (
+          <div
+            style={{
+              background: color,
+              color: '#fff',
+              padding: 'var(--ds-size-3) var(--ds-size-3)',
+              display: 'flex',
+              width: 'var(--ds-size-22)',
+              minHeight: '7rem',
+              justifySelf: 'stretch',
+              borderRadius: 'var(--ds-border-radius-md)',
+            }}
+          >
+            <Paragraph data-size="xs">{color}</Paragraph>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const DatavisSeqDivColorDisplay = () => {
+  return (
+    <div style={{ marginBlockEnd: 'var(--ds-size-8)' }}>
+      <Heading
+        data-size="sm"
+        level={3}
+        style={{ marginBlockEnd: 'var(--ds-size-4)' }}
+      >
+        Alternativ 1
+      </Heading>
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          gap: 'var(--ds-size-3)',
+          flexWrap: 'wrap',
+        }}
+      >
+        {datavisSeqDivergentColors.map((color) => (
+          <div
+            style={{
+              background: color,
+              color: color === '#DEDEDE' ? '#000' : '#fff',
+              padding: 'var(--ds-size-3) var(--ds-size-3)',
+              display: 'flex',
+              width: 'var(--ds-size-22)',
+              minHeight: '7rem',
+              justifySelf: 'stretch',
+              borderRadius: 'var(--ds-border-radius-md)',
+            }}
+          >
+            <Paragraph data-size="xs">{color}</Paragraph>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const DatavisGradient = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      <Heading
+        data-size="sm"
+        level={3}
+        style={{ marginBlockEnd: 'var(--ds-size-4)' }}
+      >
+        Alternativ 2
+      </Heading>
+      <div
+        style={{
+          minHeight: '5rem',
+          borderRadius: 'var(--ds-border-radius-md)',
+          backgroundImage: `linear-gradient(to right, ${datavisSeqMonoColors[0]}, ${datavisSeqMonoColors[7]})`,
+        }}
+      />
+    </div>
+  );
+};
+
+export const DatavisDivGradient = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      <Heading
+        data-size="sm"
+        level={3}
+        style={{ marginBlockEnd: 'var(--ds-size-4)' }}
+      >
+        Alternativ 2
+      </Heading>
+      <div
+        style={{
+          minHeight: '5rem',
+          borderRadius: 'var(--ds-border-radius-md)',
+          backgroundImage: `linear-gradient(to right, ${datavisSeqDivergentColors[0]}, ${datavisSeqDivergentColors[1]}, ${datavisSeqDivergentColors[2]}, ${datavisSeqDivergentColors[3]}, ${datavisSeqDivergentColors[4]}, ${datavisSeqDivergentColors[5]}, ${datavisSeqDivergentColors[6]})`,
+        }}
+      />
+    </div>
+  );
+};
