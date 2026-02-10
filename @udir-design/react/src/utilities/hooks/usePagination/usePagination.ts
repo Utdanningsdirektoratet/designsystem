92ff42261c9e2b0884921d7bbdbeed9a2df6ea65
Adrian Flatner <adrianrflatner@gmail.com>
2026-02-10 10:36:28 +0100

    refactor(usePagination): remove unnecessary ternary

--- @udir-design/react/src/utilities/hooks/usePagination/usePagination.ts
+++ @udir-design/react/src/utilities/hooks/usePagination/usePagination.ts
@@ -10,25 +10,19 @@ export type { UsePaginationProps };
 // Fixes accessibility issues and communicates unavailable state instead of hiding controls: https://jira.udir.no/secure/RapidBoard.jspa?rapidView=534&projectKey=DESIGN&view=detail&selectedIssue=DESIGN-472#
 export const usePagination = (props: UsePaginationProps) => {
   const pagination = useDigdirPagination(props);
-
   return useMemo(() => {
     return {
       ...pagination,
-      prevButtonProps: pagination.prevButtonProps
-        ? {
-            ...pagination.prevButtonProps,
-            'aria-hidden': undefined,
-            'aria-disabled': !pagination.hasPrev || undefined,
-          }
-        : pagination.prevButtonProps,
-
-      nextButtonProps: pagination.nextButtonProps
-        ? {
-            ...pagination.nextButtonProps,
-            'aria-hidden': undefined,
-            'aria-disabled': !pagination.hasNext || undefined,
-          }
-        : pagination.nextButtonProps,
+      prevButtonProps: {
+        ...pagination.prevButtonProps,
+        'aria-hidden': undefined,
+        'aria-disabled': !pagination.hasPrev || undefined,
+      },
+      nextButtonProps: {
+        ...pagination.nextButtonProps,
+        'aria-hidden': undefined,
+        'aria-disabled': !pagination.hasNext || undefined,
+      },
     };
   }, [pagination]);
 };
