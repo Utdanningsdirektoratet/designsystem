06fa1dfaaae2fb47ac8f51cf334fbb5ebdb2fdae
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-02-20 15:01:57 +0100

    feat: add nynorsk support to FileUpload and document existing nynorsk support in FieldNecessity

--- @udir-design/react/.storybook/docs/components/CssVariables/CssLanguageVariables.tsx
+++ @udir-design/react/.storybook/docs/components/CssVariables/CssLanguageVariables.tsx
@@ -1,34 +1,77 @@
 import { Unstyled } from '@storybook/addon-docs/blocks';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Table } from 'src/components/table';
 import styles from './CssLanguageVariables.module.css';
 
 export const CssLanguageVariables = ({
   variables,
 }: {
-  variables: Record<string, { nb: string; en: string }>;
+  variables: string[];
 }) => {
+  const langs = useMemo(() => ['nb', 'nn', 'en'] as const, []);
+  type Lang = (typeof langs)[number];
+  const enRef = useRef<HTMLDivElement>(null);
+  const nbRef = useRef<HTMLDivElement>(null);
+  const nnRef = useRef<HTMLDivElement>(null);
+  const refs = useMemo(
+    () => ({
+      en: enRef,
+      nb: nbRef,
+      nn: nnRef,
+    }),
+    [],
+  );
+  const [computedStyle, setComputedStyle] = useState<
+    Record<Lang, CSSStyleDeclaration | undefined>
+  >({
+    en: undefined,
+    nb: undefined,
+    nn: undefined,
+  });
+
+  useEffect(() => {
+    const setForLang = (lang: Lang) =>
+      setComputedStyle((prev) =>
+        !prev[lang] && refs[lang].current
+          ? { ...prev, [lang]: getComputedStyle(refs[lang].current) }
+          : prev,
+      );
+
+    langs.forEach(setForLang);
+  }, [langs, refs]);
+
+  const getTranslation = (lang: Lang, variable: string) => {
+    const langStyle = computedStyle[lang];
+    if (!langStyle) return;
+    return langStyle.getPropertyValue(variable);
+  };
+
   return (
     <Unstyled>
+      {langs.map((lang) => (
+        <div style={{ display: 'none' }} lang={lang} ref={refs[lang]} />
+      ))}
+
       <Table className={styles.languageVariables} border zebra>
         <Table.Head>
           <Table.Row>
             <Table.HeaderCell>Navn</Table.HeaderCell>
-            <Table.HeaderCell>
-              <code>lang="nb"</code>
-            </Table.HeaderCell>
-            <Table.HeaderCell>
-              <code>lang="en"</code>
-            </Table.HeaderCell>
+            {langs.map((lang) => (
+              <Table.HeaderCell>
+                <code>lang="{lang}"</code>
+              </Table.HeaderCell>
+            ))}
           </Table.Row>
         </Table.Head>
         <Table.Body>
-          {Object.entries(variables).map(([name, value]) => (
-            <Table.Row key={name}>
+          {variables.map((variable) => (
+            <Table.Row key={variable}>
               <Table.Cell>
-                <code>{name}</code>
+                <code>{variable}</code>
               </Table.Cell>
-              <Table.Cell>"{value.nb}"</Table.Cell>
-              <Table.Cell>"{value.en}"</Table.Cell>
+              {langs.map((lang) => (
+                <Table.Cell>{getTranslation(lang, variable) ?? ''}</Table.Cell>
+              ))}
             </Table.Row>
           ))}
         </Table.Body>
