9d6273cd23988c9cfc89d86c168f40308062d1ef
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-03-02 17:27:07 +0100

    docs: move large data visualization code examples to separate files that can be typechecked

---
+++ @udir-design/react/src/utilities/dataVisualization/docs/chartjsExample.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-restricted-imports */
+//@ts-expect-error example code
+import { Pie } from 'react-chartjs-2';
+import { getCategoricalColors } from '@udir-design/react/utilities/dataVisualization/alpha';
+
+export function MyChart() {
+  const data = {
+    labels: ['Norsk', 'Matematikk', 'Engelsk'],
+    datasets: [
+      {
+        data: [85, 72, 91],
+        backgroundColor: getCategoricalColors(),
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  return <Pie data={data} />;
+}
---
+++ @udir-design/react/src/utilities/dataVisualization/docs/highchartsSequentialExample.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-restricted-imports */
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import { getHighchartsTheme } from '@udir-design/react/utilities/dataVisualization/alpha';
+import { getSequentialMonochromaticColors } from '@udir-design/react/utilities/dataVisualization/alpha';
+import '@udir-design/theme/dataviz.css';
+import 'highcharts/i18n/nb-NO'; // Norsk språkpakke
+
+Highcharts.setOptions(getHighchartsTheme());
+const seq = getSequentialMonochromaticColors();
+
+export function MyChart() {
+  const options: Highcharts.Options = {
+    chart: { type: 'heatmap' },
+    colorAxis: {
+      min: 0,
+      max: 100,
+      stops: [
+        [0.0, seq[0]],
+        [0.2, seq[1]],
+        [0.4, seq[2]],
+        [0.6, seq[3]],
+        [0.8, seq[4]],
+        [1.0, seq[5]],
+      ],
+    },
+    series: [
+      {
+        type: 'heatmap',
+        data: [
+          [0, 0, 12],
+          [1, 0, 55],
+          [2, 0, 90],
+        ],
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+}
