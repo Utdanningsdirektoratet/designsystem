bc8eabb6c6f319aa9b7cd16ae68e0f426bb41d10
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-03-02 16:07:21 +0100

    build: import dataviz colors in nextjs test-app

--- test-apps/nextjs/src/app/layout.tsx
+++ test-apps/nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import '@udir-design/react/style.css';
+import '@udir-design/theme/dataviz.css';
 import './global.css';
 import { TestFooter as Footer } from './components/TestFooter';
 import { TestHeader as Header } from './components/TestHeader';
