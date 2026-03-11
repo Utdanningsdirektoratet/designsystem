1d2ace2bab07516691402d50630a28acd297ddd0
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-03-11 11:15:03 +0100

    fix(FieldNecessity): alignment of icons in label when readOnly

--- @udir-design/react/src/components/textfield/Textfield.tsx
+++ @udir-design/react/src/components/textfield/Textfield.tsx
@@ -1,3 +1,5 @@
+/* Import Field's css here, otherwise it will not always be loaded in Storybook due to Textfield using Digdir's field directly */
+import '../field/field.css';
 import { Textfield, type TextfieldProps } from '@digdir/designsystemet-react';
 
 export type { TextfieldProps };
