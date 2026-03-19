386995a6ba43a1087df503dd17b22857384089bd
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-03-19 21:23:01 +0100

    docs(TableOfContents): ensure onToggle is documented by adding a way to explicitly include props

--- @udir-design/react/.storybook/main.ts
+++ @udir-design/react/.storybook/main.ts
@@ -2,6 +2,26 @@ import { execSync } from 'node:child_process';
 import { defineMain } from '@storybook/react-vite/node';
 import remarkGfm from 'remark-gfm';
 import type { Plugin, UserConfig } from 'vite';
+// eslint-disable-next-line no-restricted-imports
+import type * as alphaComponents from '../src/components/alpha';
+// eslint-disable-next-line no-restricted-imports
+import type * as betaComponents from '../src/components/beta';
+// eslint-disable-next-line no-restricted-imports
+import type * as stableComponents from '../src/components/stable';
+
+type ComponentNames =
+  | keyof typeof alphaComponents
+  | keyof typeof betaComponents
+  | keyof typeof stableComponents;
+
+const explicitlyIncludedProps: Record<string, string[] | undefined> = {
+  TableOfContents: ['onToggle'],
+} satisfies Partial<Record<ComponentNames, string[]>>;
+
+const isFromDependency = (fileName: string) =>
+  fileName.includes('node_modules');
+const isFromAllowedDependency = (fileName: string) =>
+  ['@digdir', '@udir-design'].some((org) => fileName.includes(org));
 
 export default defineMain({
   stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
@@ -85,6 +105,29 @@ export default defineMain({
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
+      propFilter: (prop, component) => {
+        // Make it possible to explicitly choose to include a prop that would otherwise be
+        // filtered out because it's a third-party prop, e.g. types from React
+        if (explicitlyIncludedProps[component.name]?.includes(prop.name)) {
+          return true;
+        }
+
+        // Remove popovertarget prop which @digdir/designsystemet-react adds to all elements
+        if (prop.name === 'popovertarget') {
+          return false;
+        }
+
+        // Filter out third-party props from node_modules except @digdir packages.
+        const filesToCheck = [
+          prop.parent?.fileName ?? [],
+          prop.declarations?.map((x) => x.fileName) ?? [],
+        ].flat();
+
+        return filesToCheck.some(
+          (fileName) =>
+            isFromAllowedDependency(fileName) || !isFromDependency(fileName),
+        );
+      },
       tsconfigPath: 'tsconfig.lib.json',
       // Required for unions like Size, Color etc from @digdir to generate options in Storybook controls
       shouldExtractLiteralValuesFromEnum: true,
