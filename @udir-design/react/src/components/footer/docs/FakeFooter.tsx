e39f2e630caa3c7f65623c51fc68b5cad9ff3ced
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-03-27 11:19:05 +0100

    docs(Footer): document props for subcomponents

---
+++ @udir-design/react/src/components/footer/docs/FakeFooter.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import type { FooterProps } from '../Footer';
+
+/**
+ * This component only exists because react-docgen-typescript doesn't manage to generate
+ * Storybook controls for Footer directly :(
+ */
+export const Footer: FunctionComponent<FooterProps> = () => null;
