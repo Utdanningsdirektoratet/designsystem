1a372dac0c02bf36ea19b2d080793e3f5785f6bc
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-03-27 14:16:59 +0100

    docs(List): documentation of props for subcomponents

---
+++ @udir-design/react/src/components/list/docs/FakeListItem.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from 'react';
+import type { ListItemProps } from '../List';
+
+/**
+ * This component only exists to add relevant html props for ListItem
+ */
+export const ListItem: FunctionComponent<ListItemProps> = () => null;
