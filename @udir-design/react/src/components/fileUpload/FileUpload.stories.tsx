06c2cbadc98c242ec983927783f08bc1f7d91502
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-03-30 11:20:34 +0200

    refactor(FileUpload): changes to type to match new web components structure

--- @udir-design/react/src/components/fileUpload/FileUpload.stories.tsx
+++ @udir-design/react/src/components/fileUpload/FileUpload.stories.tsx
@@ -138,7 +138,7 @@ export const ExampleDropZone = meta.story({
           label="Last opp dokumentasjon"
           description="Du kan laste opp filer i PDF-format. Filer kan være opptil 0.5 MB."
           inputProps={{
-            ...getInputProps(),
+            ...getInputProps({ multiple: true }),
             id: 'dokumentasjon',
           }}
           isDragGlobal={isDragGlobal}
@@ -422,15 +422,16 @@ export const Upload = meta.story({
     const [file, setFile] = useState<File | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
-    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-      const files = e.target.files; // safe
+    const handleOnChange = (e: ChangeEvent<HTMLElement>) => {
+      const input = e.target as HTMLInputElement;
+      const files = input.files; // safe
 
       if (files) {
         setFile(files[0]);
         setLoading(true);
         setTimeout(() => {
           setLoading(false);
-          e.target.value = '';
+          input.value = '';
         }, 2000);
       }
     };
