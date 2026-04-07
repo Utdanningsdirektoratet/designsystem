144ac896c32e1cd28ce5149b5a8c9a8f3a049dc6
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-07 10:45:22 +0200

    docs(Search): props for subcomponents

---
+++ @udir-design/react/src/components/search/docs/FakeSearchButton.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from 'react';
+import type { SearchButtonProps } from '../Search';
+
+/**
+ * This component only exists to add relevant html props for SearchButton
+ */
+export const SearchButton: FunctionComponent<SearchButtonProps> = () => null;
