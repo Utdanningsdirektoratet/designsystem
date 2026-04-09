6ba9878ee579489a3f7f4bc586a41b9989a836ec
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-09 10:38:27 +0200

    docs(Select): props for subcomponents

--- @udir-design/react/src/components/select/Select.stories.tsx
+++ @udir-design/react/src/components/select/Select.stories.tsx
@@ -8,9 +8,16 @@ import { Heading } from '../typography/heading/Heading';
 import { Label } from '../typography/label/Label';
 import { ValidationMessage } from '../typography/validationMessage/ValidationMessage';
 import { Select } from './Select';
+import { Select as FakeSelect } from './docs/FakeSelect';
+import { SelectOptgroup } from './docs/FakeSelectOptgroup';
+import { SelectOption } from './docs/FakeSelectOption';
 
 const meta = preview.meta({
-  component: Select,
+  component: FakeSelect,
+  subcomponents: {
+    'Select.Option': SelectOption,
+    'Select.Optgroup': SelectOptgroup,
+  },
   tags: ['beta', 'digdir'],
   parameters: {
     componentOrigin: {
