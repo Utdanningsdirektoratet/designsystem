e7222854d96186b6334cb1d96dedb7f863d11e53
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-09 11:56:24 +0200

    docs(Tabs): props for subcomponents

---
+++ @udir-design/react/src/components/tabs/docs/FakeTabs.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import type { TabsProps } from '../Tabs';
+
+/**
+ * This component only exists because react-docgen-typescript doesn't manage to generate
+ * Storybook controls for Tabs directly :(
+ */
+export const Tabs: FunctionComponent<TabsProps> = () => null;
