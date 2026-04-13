50c6e2dc79c2bde87c651ff0b2d8ebec250a763f
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-13 12:50:47 +0200

    docs: add skiplink and update title vite testapp

--- test-apps/vite/src/App.tsx
+++ test-apps/vite/src/App.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import { DemoBanner } from '@udir-design/react/beta';
+import { DemoBanner, SkipLink } from '@udir-design/react/beta';
 import { TestFooter as Footer } from './components/TestFooter';
 import { TestHeader as Header } from './components/TestHeader';
 
 function App() {
   return (
     <DemoBanner>
+      <SkipLink href="#main-content">Hopp til hovedinnholdet</SkipLink>
       <Header />
-      <main className="content">
+      <main className="content" id="main-content">
         <Outlet />
       </main>
       <Footer />
