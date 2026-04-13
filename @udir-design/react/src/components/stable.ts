5cd8215b4ee6b440884be5cb0cd67983d70ace74
Adrian Flatner <adrianrflatner@gmail.com>
2026-04-13 10:25:02 +0200

    feat(Avatar): promote from beta to stable

--- @udir-design/react/src/components/stable.ts
+++ @udir-design/react/src/components/stable.ts
@@ -2,6 +2,7 @@
  * This file exports the components that are deemed stable (not alpha or beta)
  */
 
+export * from './avatar/Avatar';
 export * from './checkbox/Checkbox';
 export * from './divider/Divider';
 export * from './footer';
