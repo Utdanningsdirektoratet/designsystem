b02e436b6ff064015c10845786f64b836e1feb70
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-14 14:41:38 +0200

    docs: data-show on correct compontent to fix gap

--- @udir-design/react/src/demo/DashboardDemo.stories.tsx
+++ @udir-design/react/src/demo/DashboardDemo.stories.tsx
@@ -101,7 +101,7 @@ export const DashboardStory = meta.story({
               <Dropdown.Item>
                 <Dropdown.Heading>Detaljer</Dropdown.Heading>
               </Dropdown.Item>
-              <Dropdown.Item>
+              <Dropdown.Item style={{ marginLeft: 'var(--ds-size-4)' }}>
                 <Paragraph>Stian Hansen</Paragraph>
               </Dropdown.Item>
               <Dropdown.Item
