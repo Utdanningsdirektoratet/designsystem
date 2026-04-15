36272061c9bc14c712b5958f845634807dc65ac0
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 09:08:28 +0200

    docs(FileUpload): docs for subcomponents

---
+++ @udir-design/react/src/components/fileUpload/docs/FakeFileUploadItem.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import type { FileUploadItemProps } from '..';
+
+/**
+ * This component only exists to add relevant html props for FileUpload.Item
+ */
+export const FileUploadItem: FunctionComponent<FileUploadItemProps> = () =>
+  null;
