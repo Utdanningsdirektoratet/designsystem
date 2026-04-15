e3cbc563cfcc1f78bc9c76d175dbefbddb3f8d2c
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 09:08:27 +0200

    docs(Search): props for subcomponents

---
+++ @udir-design/react/src/components/search/docs/FakeSearchClear.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from 'react';
+import type { SearchClearProps } from '../Search';
+
+/**
+ * This component only exists to add relevant html props for SearchClear
+ */
+export const SearchClear: FunctionComponent<SearchClearProps> = () => null;
