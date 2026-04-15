9cd8c7dc036526010c5fd404a688fc5ae6d8b13e
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 09:08:27 +0200

    docs(Select): props for subcomponents

---
+++ @udir-design/react/src/components/select/docs/FakeSelectOptgroup.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import type { SelectOptgroupProps } from '../Select';
+
+/**
+ * This component only exists to add relevant html props for SelectOptgroup
+ */
+export const SelectOptgroup: FunctionComponent<SelectOptgroupProps> = () =>
+  null;
