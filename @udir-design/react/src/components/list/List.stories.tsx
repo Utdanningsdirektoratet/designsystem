53060ee7859d18af678d793bb8857e7639d4d259
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 11:08:14 +0200

    chore: fix display name in code example and controller tabs

--- @udir-design/react/src/components/list/List.stories.tsx
+++ @udir-design/react/src/components/list/List.stories.tsx
@@ -4,10 +4,19 @@ import { Heading } from '../typography/heading/Heading';
 import { List } from './List';
 import { ListItem } from './docs/FakeListItem';
 import { ListOrdered } from './docs/FakeListOrdered';
+import { ListUnordered } from './docs/FakeListUnordered';
+
+// Hack to get the first tab in Controls to have the correct name List.Unordered instead of ListUnordered
+if (Object.hasOwn(ListUnordered, '__docgenInfo')) {
+  (
+    ListUnordered as unknown as {
+      __docgenInfo: { displayName: string };
+    }
+  ).__docgenInfo.displayName = 'List.Unordered';
+}
 
 const meta = preview.meta({
-  // Displays as ListUnordered
-  component: List.Unordered,
+  component: ListUnordered,
   subcomponents: {
     'List.Ordered': ListOrdered, // Not subcomponent but want it to be displayed in tab-view for props
     'List.Item': ListItem,
@@ -83,7 +92,7 @@ export const Indented = meta.story({
   args: {
     style: { marginTop: 'var(--ds-size-2)' },
   },
-  render: ({ ref: _ref, ...args }) => (
+  render: (args) => (
     <>
       <Heading level={2} data-size="xs">
         Innhold
