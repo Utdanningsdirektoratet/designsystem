bdddc948b5066d8a4647a5ccd6747102abf2e258
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 09:08:26 +0200

    docs(Dropdown): document props

---
+++ @udir-design/react/src/components/dropdown/docs/FakeDropdownHeading.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import type { DropdownHeadingProps } from '../Dropdown';
+
+/**
+ * This component only exists to add relevant html props for DropdownHeading
+ */
+export const DropdownHeading: FunctionComponent<DropdownHeadingProps> = () =>
+  null;
