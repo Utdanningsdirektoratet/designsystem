4498fb4d4acb4ecafa2c2cf4bddeb9e48a6044c2
Hanne Sofie Haugland <hanne.haugland@bekk.no>
2026-04-15 09:08:26 +0200

    docs(ErrorSummary): document props

---
+++ @udir-design/react/src/components/errorSummary/docs/FakeErrorSummaryHeading.tsx
@@ -0,0 +1,9 @@
+import type { FunctionComponent } from 'react';
+import type { ErrorSummaryHeadingProps } from '../ErrorSummary';
+
+/**
+ * This component only exists to add relevant html props for ErrorSummaryHeading
+ */
+export const ErrorSummaryHeading: FunctionComponent<
+  ErrorSummaryHeadingProps
+> = () => null;
