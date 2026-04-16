614fafc1f68cd81e6c25040089491ec69bce1142
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-04-16 15:33:05 +0200

    chore(test-apps/vite-vanilla): don't require js to run before static icons and symbols show up

--- test-apps/vite-vanilla/src/index.js
+++ test-apps/vite-vanilla/src/index.js
@@ -5,18 +5,6 @@ import '@udir-design/icons/style.css';
 import '@udir-design/icons/css/lightBulb.css';
 
 import ArrowRightUrl from '@udir-design/icons/svg/ArrowRight.svg?no-inline';
-import PencilWritingUrl from '@udir-design/icons/svg/PencilWriting.svg?no-inline';
-import DatamaskinFillUrl from '@udir-design/symbols/svg/DatamaskinFill.svg?url';
-
-document
-  .getElementById('icon-pencil')
-  ?.setAttribute('href', `${PencilWritingUrl}#icon`);
-document
-  .getElementById('symbol-computer')
-  ?.setAttribute('src', DatamaskinFillUrl);
-document
-  .getElementById('icon-arrow-link')
-  ?.setAttribute('href', `${ArrowRightUrl}#icon`);
 
 const container = document.getElementById('app');
 if (container) {
--- test-apps/vite-vanilla/vite.config.js
+++ test-apps/vite-vanilla/vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const icons = ['ArrowRight', 'PencilWriting'];
+const symbols = ['DatamaskinFill'];
 
 export default defineConfig({
   root: 'src',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: icons.map(
+            (icon) => `../node_modules/@udir-design/icons/dist/svg/${icon}.svg`,
+          ),
+          dest: 'assets/icons/',
+          rename: { stripBase: true },
+        },
+        {
+          src: symbols.map(
+            (symbol) =>
+              `../node_modules/@udir-design/symbols/dist/svg/${symbol}.svg`,
+          ),
+          dest: 'assets/symbols/',
+          rename: { stripBase: true },
+        },
+      ],
+    }),
+  ],
   server: {
     port: 4200,
     host: 'localhost',
