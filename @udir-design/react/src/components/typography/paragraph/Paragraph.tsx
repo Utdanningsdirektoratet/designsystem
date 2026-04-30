c0a8ac46aa44b098bf10eb282aee066cdae43dc9
Une Sofie Kinn Ekroll <une.kinn.ekroll@bekk.no>
2026-04-30 12:22:59 +0200

    fix(Paragraph): remove "variant" prop
    We had no guidelines on when to use "long" or "short" variant, and are
    of the opinion that we should rather have more semantic typography
    components in the future, with proper guidelines.
    
    BREAKING CHANGE: if you used `variant="long"`, `"short"` or `"default"`,
    you must remove the prop usage.

--- @udir-design/react/src/components/typography/paragraph/Paragraph.tsx
+++ @udir-design/react/src/components/typography/paragraph/Paragraph.tsx
@@ -1,4 +1,20 @@
-import { Paragraph, type ParagraphProps } from '@digdir/designsystemet-react';
+import {
+  Paragraph as DigdirParagraph,
+  type ParagraphProps as DigdirParagraphProps,
+} from '@digdir/designsystemet-react';
+import type {
+  ForwardRefExoticComponent,
+  HTMLAttributes,
+  RefAttributes,
+} from 'react';
+
+type ParagraphProps = Omit<DigdirParagraphProps, 'variant'>;
+
+const Paragraph = DigdirParagraph as ForwardRefExoticComponent<
+  ParagraphProps &
+    HTMLAttributes<HTMLParagraphElement> &
+    RefAttributes<HTMLParagraphElement>
+>;
 
 export type { ParagraphProps };
 export { Paragraph };
