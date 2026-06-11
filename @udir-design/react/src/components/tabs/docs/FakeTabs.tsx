03e1142f13ccd0e2c2cd774631ddbb3c20459147
Adrian Flatner <adrianrflatner@gmail.com>
2026-06-11 08:49:25 +0200

    docs: formatting

--- @udir-design/react/src/components/tabs/docs/FakeTabs.tsx
+++ @udir-design/react/src/components/tabs/docs/FakeTabs.tsx
@@ -1,7 +1,6 @@
 import type { FunctionComponent } from 'react';
 import type { TabsProps } from '../Tabs';
 
-
 /**
  * This component only exists because react-docgen-typescript doesn't manage to generate
  * Storybook controls for Tabs directly :(
