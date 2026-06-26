5ea351afee513dd92dcec3dd854e1e48004cee1f
hannareie <hannahreiestad@gmail.com>
2026-06-26 13:38:43 +0200

    feat: update sequential color palette

--- @udir-design/react/src/utilities/datavis/dataVisualisation.spec.ts
+++ @udir-design/react/src/utilities/datavis/dataVisualisation.spec.ts
@@ -106,9 +106,15 @@ describe('generateSequentialMonochromaticColors', () => {
       const r = srgbToLinear((n >> 16) & 0xff);
       const g = srgbToLinear((n >> 8) & 0xff);
       const b = srgbToLinear(n & 0xff);
-      const l = Math.cbrt(0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b);
-      const m = Math.cbrt(0.2119034982 * r + 0.6806995451 * g + 0.1073969566 * b);
-      const s = Math.cbrt(0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b);
+      const l = Math.cbrt(
+        0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b,
+      );
+      const m = Math.cbrt(
+        0.2119034982 * r + 0.6806995451 * g + 0.1073969566 * b,
+      );
+      const s = Math.cbrt(
+        0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b,
+      );
       return 0.2104542553 * l + 0.793617785 * m - 0.0040720468 * s;
     };
 
